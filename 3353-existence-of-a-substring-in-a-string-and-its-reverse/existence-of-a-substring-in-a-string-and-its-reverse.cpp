16c69dd82cefd8b460e65ebf7d072e6a65b2339e
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-03-17 16:51:55 +0530

    Time: 5 ms (33.33%) | Memory: 7.6 MB (66.67%) - LeetSync

---
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string &s,int st,int e){
+        int i = st, j = e;
+        while(i<j){
+            if(s[i]!=s[j])
+            return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool isSubstringPresent(string s) {
+        if(s.size()<2) return false;
+        for(int i=0;i<s.size();i++){
+            for(int j=i+1;j<s.size();j++){
+                if(isPalindrome(s,i,j))
+                return true;
+            }
+        }
+        return false;
+       
+    }
+};
