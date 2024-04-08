a57aff1bc0dcbc2ddd41e6c44c60f63f7dc99667
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-04-08 19:50:11 +0530

    Time: 48 ms (26.30%) | Memory: 46.6 MB (35.02%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -16,10 +16,29 @@ public:
 
         return dp[idx][amount] = min(pick,notpick);
     }
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& coins, int Amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = f(n-1,amount,coins,dp);
-        return ans>=1e9?-1:ans;
+        vector<vector<int>>dp(n,vector<int>(Amount+1,-1));
+        // int ans = f(n-1,amount,coins,dp);
+        // return ans>=1e9?-1:ans;
+
+        for(int i=0;i<n;i++){
+            dp[i][0] = 0;
+        }
+        for(int i =0;i<=Amount;i++){
+            dp[0][i] = i%coins[0] == 0? i/coins[0] : 1e9;
+        }
+
+        for(int idx =1;idx<n;idx++){
+            for(int amount = 1;amount<=Amount;amount++){
+                int notpick = 0 + dp[idx-1][amount];// f(idx-1,amount,coins,dp);
+                int pick = INT_MAX;
+                if(coins[idx]<=amount)
+                pick = 1 + dp[idx][amount-coins[idx]];// f(idx,amount-coins[idx],coins,dp);
+
+                dp[idx][amount] = min(pick,notpick);
+            }
+        }
+        return dp[n-1][Amount]>=1e9 ? -1 : dp[n-1][Amount];
     }
 };
