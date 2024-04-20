f43fef29a85cda4e5f0e5ff7ab9f4ea5c91d8d0e
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-04-20 10:48:58 +0530

    Time: 129 ms (85.58%) | Memory: 66.9 MB (75.13%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    struct comp{
-        bool operator()(pair<int,int>&p1, pair<int,int>&p2){
-            return p1.first*p1.first + p1.second*p1.second > p2.first*p2.first + p2.second*p2.second;
-        }
-    };
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<pair<int,int>>v;
+        priority_queue<pair<int,pair<int,int>>>pq; // max heap
         for(int i=0;i<points.size();i++){
             int x = points[i][0];
             int y = points[i][1];
-            v.push_back({x,y});
-        }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,comp> pq(v.begin(),v.end());
-        vector<vector<int>>ans;
-        for(int i=0;i<k;i++){
-            ans.push_back({pq.top().first,pq.top().second});
+            int d = x*x + y*y;
+            pq.push({d,{x,y}});
+            if(pq.size()>k)
             pq.pop();
         }
-    return ans;
+        vector<vector<int>>ans;
+        while(!pq.empty()){
+            ans.push_back({pq.top().second.first,pq.top().second.second});
+            pq.pop();
+        }
+        return ans;
     }
 };
