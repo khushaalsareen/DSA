268a94db45695684a2c63e1d5399ae6c7cb49e84
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-04-30 01:21:34 +0530

    Time: 140 ms (50.42%) | Memory: 62.4 MB (53.14%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-   vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-    vector<vector<int>> reverseGraph(graph.size()); // Reverse graph to store incoming edges
-    vector<int> outDegree(graph.size()); // Out degree of each node
-    
-    // Construct reverse graph and compute out degrees
-    for (int i = 0; i < graph.size(); ++i) {
-        for (int j : graph[i]) {
-            reverseGraph[j].push_back(i); // Add incoming edge
-            outDegree[i]++; // Increment out degree
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int>indegree(graph.size(),0);
+        vector<int>adjLs[graph.size()];
+        for(int i=0;i<graph.size();i++){
+            for(int j=0;j<graph[i].size();j++){
+                indegree[i]++;
+                adjLs[graph[i][j]].push_back(i);
+            }
         }
-    }
-    
-    queue<int> q;
-    
-    // Initialize queue with nodes having out degree 0 (safe nodes)
-    for (int i = 0; i < graph.size(); ++i) {
-        if (outDegree[i] == 0)
-            q.push(i);
-    }
-    
-    vector<int> result;
-    
-    // Perform BFS starting from nodes with out degree 0
-    while (!q.empty()) {
-        int node = q.front();
-        q.pop();
-        result.push_back(node); // Node is safe
-        for (int neighbor : reverseGraph[node]) {
-            outDegree[neighbor]--; // Decrement out degree of neighbors
-            if (outDegree[neighbor] == 0)
-                q.push(neighbor); // If out degree becomes 0, neighbor is safe
+        queue<int>q;
+        vector<int>ans;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
         }
+        while(!q.empty()){
+            int currNode = q.front();
+            q.pop();
+            ans.push_back(currNode);
+            for(auto it:adjLs[currNode]){
+                indegree[it]--;
+                if(indegree[it]==0)
+                q.push(it);
+            }
+        }
+        sort(ans.begin(),ans.end());
+            return ans;
     }
-    
-    sort(result.begin(), result.end()); // Sort the result
-    return result;
-}
-
 };
