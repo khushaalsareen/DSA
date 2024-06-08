d5e80d7bb09910b5a980076fe83f43b7516dd8e5
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-06-08 15:31:32 +0530

    Time: 5 ms (36.52%) | Memory: 10.3 MB (8.12%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-    int f(int i,int j,vector<vector<int>>&obstacleGrid,vector<vector<int>>&dp){
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1) return 0;
-        if(i==0 && j==0) return 1;
+     int f(int i, int j, int m, int n,vector<vector<int>>&dp,vector<vector<int>>& obstacleGrid){
         if(i<0 || j<0) return 0;
+        if(obstacleGrid[i][j]==1)
+        return 0;
+        if(i==0 && j==0) return 1;
+        
+        
         if(dp[i][j]!=-1) return dp[i][j];
-        int up = f(i-1,j,obstacleGrid,dp);
-        int left = f(i,j-1,obstacleGrid,dp);
-        return dp[i][j] = up + left;
+        int left = f(i,j-1,m,n,dp,obstacleGrid);
+        int up = f(i-1,j,m,n,dp,obstacleGrid);
+        return dp[i][j]= left + up;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-         int m = obstacleGrid.size();
-         int n = obstacleGrid[0].size();
-         vector<vector<int>>dp(m,vector<int>(n,0));
-        //  return f(m-1,n-1,obstacleGrid,dp);
-        dp[0][0] = 1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(obstacleGrid[i][j]==1) dp[i][j] = 0;
-                else if(i==0 && j==0) continue;
-                else
-                {
-                 int up = 0, left = 0;
-                 if(i!=0)
-                 up = dp[i-1][j];//  (i-1,j,obstacleGrid,dp);
-                 if(j!=0)
-                 left =dp[i][j-1]; // f(i,j-1,obstacleGrid,dp);
-                dp[i][j] = up + left; 
-                }
-
-            }
-        }
-        return dp[m-1][n-1];
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return f(m-1,n-1,m,n,dp,obstacleGrid);
     }
 };
