726f8f3531905555310e68c9779431d1c2725230
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-06-08 17:02:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (56.14%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -10,7 +10,19 @@ public:
         return dp[i][j] = min(down,right);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>>dp(triangle.size(),vector<int>(triangle.size(),-1));
-        return f(0,0,triangle,dp);
+        int n = triangle.size();
+        vector<vector<int>>dp(triangle.size(),vector<int>(triangle.size(),0));
+        // return f(0,0,triangle,dp);
+        for(int j=0;j<triangle.size();j++){
+            dp[triangle.size()-1][j] = triangle[triangle.size()-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=triangle[i].size()-1;j>=0;j--){
+                int down = triangle[i][j] + dp[i+1][j];
+                 int right = triangle[i][j] + dp[i+1][j+1];// f(i+1,j+1,triangle,dp);
+                dp[i][j] = min(down,right);
+            }
+        }
+        return dp[0][0];
     }
 };
