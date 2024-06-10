fc59e640dbfd229311cedc89bab50d12170b8da2
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-06-10 12:53:39 +0530

    Time: 23 ms (64.44%) | Memory: 25.6 MB (52.20%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -6,6 +6,7 @@ public:
         if(dp[ind1+1][ind2+1]!=-1) return dp[ind1+1][ind2+1];
         if(text1[ind1]==text2[ind2])
         return 1 + f(ind1-1, ind2-1,text1,text2,dp);
+        else
         return dp[ind1+1][ind2+1]= max(f(ind1-1,ind2,text1,text2,dp), f(ind1,ind2-1,text1,text2,dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
@@ -19,11 +20,11 @@ public:
         for(int i=0;i<text1.size();i++){
             dp[i][-1+1] = 0;
         }
-        for(int ind1 = 0;ind1<text1.size();ind1++){
-            for(int ind2 = 0; ind2<text2.size(); ind2++){
-                if(text1[ind1]==text2[ind2])
-                dp[ind1+1][ind2+1] = 1 + dp[ind1][ind2];// f(ind1-1, ind2-1,text1,text2,dp);
-                else dp[ind1+1][ind2+1]= max(dp[ind1+1][ind2], dp[ind1][ind2+1]);// f(ind1,ind2-1,text1,text2,dp));
+        for(int ind1 = 1;ind1<=text1.size();ind1++){
+            for(int ind2 = 1; ind2<=text2.size(); ind2++){
+                if(text1[ind1-1]==text2[ind2-1])
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];// f(ind1-1, ind2-1,text1,text2,dp);
+                else dp[ind1][ind2]= max(dp[ind1][ind2-1], dp[ind1-1][ind2]);// f(ind1,ind2-1,text1,text2,dp));
             }
         }
         return dp[sz1][sz2];
