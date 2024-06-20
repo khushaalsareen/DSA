e53512153dc67e36e334d4ee04d0680f5dc0a52b
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-06-20 15:28:22 +0530

    Time: 4 ms (99.71%) | Memory: 26.6 MB (70.31%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -5,12 +5,12 @@ public:
         int freq= 0;
         int i =0;
         while(i<nums.size()){
-            if(freq==0){
+            if(nums[i]==maj)
+            freq++;
+            else if(freq==0){
                 maj = nums[i];
                 freq = 1;
             }
-            else if(maj == nums[i])
-            freq++;
             else
             freq--;
             i++;
