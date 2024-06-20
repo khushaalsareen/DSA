aa1c64661ed3aa5d10184cf950f3571f9367c489
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-06-20 13:59:52 +0530

    Time: 7 ms (84.85%) | Memory: 14.4 MB (12.60%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
-    int binarySearch(vector<pair<int,int>>&nums, int l, int h, int target){
-        int mid ;
-        while(l<=h){
-            mid = l+(h-l)/2;
-        if(nums[mid].first==target)
-        return nums[mid].second;
-        else if(nums[mid].first>target)
-        h = mid -1;
-        else 
-        l = mid + 1;
-        } 
-        
-        return -1;
-    }
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int,int>>v;
-        for(int i=0;i<nums.size();i++){
-            v.push_back({nums[i],i});
+        unordered_map<long long int,int>m;
+        m[nums[0]] = 0;
+        for(int i=1;i<nums.size();i++){
+            if(m.find(target*1LL-nums[i])!=m.end()){
+                auto it = m.find(target - nums[i]);
+                return {i,it->second};
+            }
+            m[nums[i]*1LL]  = i;
         }
-        sort(v.begin(),v.end());
-        for(auto it:v){
-            cout<<it.first <<"  "<<it.second<<endl;
-        }
-        for(int i=0;i<nums.size();i++){
-            int rem = target - v[i].first;
-            int ans = binarySearch(v,i+1,nums.size()-1,rem);
-            if(ans!=-1)
-            return {v[i].second,ans};
-        }
-        return {-1};
+        return {};
     }
 };
