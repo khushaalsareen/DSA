548ed2cbc7c5d1e098bcbb94597c7b61e44ec86d
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-06-22 23:48:25 +0530

    Time: 231 ms (20.00%) | Memory: 227.1 MB (60.00%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int f(int mid, vector<int>&nums){
+    bool f(int mid, vector<int>nums){
         int sum = 0;
         for(auto it:nums)
         sum+=it;
         if(sum == nums.size())
-        return 0;
+        return true;
         int cnt = 0;
         for(int i=0;i<=nums.size()-3;i++){
             if(nums[i]==0){
@@ -27,19 +27,29 @@ public:
                     sum++;
                     nums[i+2] = 1;
                 }
+                if(cnt>mid)
+                return false;
             }
-        if(sum == nums.size())
-        return cnt;
         }
-        
-        return -1;
+        if(sum == nums.size())
+        return true;
+        return false;
     }
     int minOperations(vector<int>& nums) {
         int l = 0;
         int h = nums.size() - 2;
         int mid;
         int ans = -1;
-        
-        return f(0,nums);
+        while(l<=h){
+            mid = l+(h-l)/2;
+            if(f(mid,nums))
+            {
+                 ans= mid;
+                h = mid -1;
+            }
+            else
+            l = mid + 1;
+        }
+        return ans;
     }
 };
