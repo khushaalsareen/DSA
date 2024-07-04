e0ec070227117579734dc81a810256102673ab09
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-07-04 23:47:22 +0530

    Time: 66 ms (63.94%) | Memory: 19.4 MB (53.69%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,46 +1,36 @@
 class Solution {
-    bool differ(string &s1, string &s2){
-        int i = 0;
-        int j = 0;
-        int size1 = s1.size(), size2 = s2.size();
-        int cnt = 0;
-        while(i<s1.size() && j<s2.size()){
-            if(s1[i]!=s2[j])
-            cnt++;
-            i++;
-            j++;
-        }
-        if(cnt==0)
-        return abs(size1-size2) == 1;
-        return cnt==1;
-    }
+    
 public:
     int bfs(string &beginWord, string &target, vector<string>&wordList,unordered_set<string>&s){
         queue<pair<string,int>>q;
-        q.push({beginWord,0});
-        
+        q.push({beginWord,1});
+        s.erase(beginWord);
         while(!q.empty()){
             pair<string,int>p = q.front();
             q.pop();
             string &currWord = p.first;
             int steps = p.second;
             if(currWord == target)
-            return steps+1;
-            for(int i=0;i<wordList.size();i++){
-                if(s.find(wordList[i])==s.end()){
-                    if(differ(currWord,wordList[i])){
-                      cout<<wordList[i]<<" "<<steps+1<<endl;
-                       q.push({wordList[i],steps+1});
-                       s.insert(wordList[i]);
+            return steps;
+            
+            for(int i=0;i<currWord.size();i++){
+                char original = currWord[i];
+                for(char ch='a';ch<='z';ch++){
+                    currWord[i] = ch;
+                    if(s.find(currWord)!=s.end()){
+                        q.push({currWord,steps+1});
+                        s.erase(currWord);
                     }
                 }
+                currWord[i] = original;
             }
         }
+
         return 0;
     }
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         
-        unordered_set<string>s;
+       unordered_set<string>s(wordList.begin(),wordList.end());
        return bfs(beginWord, endWord,wordList, s);
     }
 };
