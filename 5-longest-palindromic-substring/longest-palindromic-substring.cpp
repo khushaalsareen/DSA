eeee595ad3e0843fd6fa3bdbf2968f82fd091afc
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-07-13 19:08:10 +0530

    Time: 7 ms (95.27%) | Memory: 18.6 MB (52.17%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -9,10 +9,12 @@ public:
     }
     string longestPalindrome(string s) {
         int n = s.size();
-        string maxi=s.substr(0,1);
-        for(int i =0;i<n-1;i++){
+        string maxi="";
+        for(int i =0;i<n;i++){
+            string even = "";
             string odd = expand(s,i,i);
-            string even = expand(s,i,i+1);
+            if(i+1<n)
+             even = expand(s,i,i+1);
             if(odd.size()>maxi.size())
             maxi = odd;
             if(even.size()>maxi.size())
