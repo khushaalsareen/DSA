0a52117b40b9c2beda2396df5a8295fff27b6841
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-07-30 16:01:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (61.08%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void generate(int ind, vector<int>&v, vector<vector<int>>&ans, vector<int>&nums){
+        if(ind == nums.size()){
+            ans.push_back(v);
+            return;
+        }
+        // take ith element
+        v.push_back(nums[ind]);
+        generate(ind+1,v,ans,nums);
+        v.pop_back();
+        // not take
+        int j;
+        for(j = ind+1;j<nums.size();j++){
+            if(nums[j]!=nums[j-1])
+            break;
+        }
+        generate(j,v,ans,nums);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>v;
+        sort(nums.begin(),nums.end());
+        generate(0,v,ans,nums);
+        return ans;
+    }
+};
