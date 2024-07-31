04ed311fdb55cd3dd06e2d6c164629d77480493c
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-07-31 15:53:12 +0530

    Time: 40 ms (23.87%) | Memory: 26.2 MB (5.84%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    int f(int ind1, int ind2, string &text1, string &text2,vector<vector<int>>&dp){
-        if(ind1<0 || ind2<0)
+    int f(int ind1, int ind2, string &str1, string &str2,vector<vector<int>>&dp){
+        if(ind1==0 || ind2==0)
         return 0;
-        if(dp[ind1+1][ind2+1]!=-1) return dp[ind1+1][ind2+1];
-        if(text1[ind1]==text2[ind2])
-        return 1 + f(ind1-1, ind2-1,text1,text2,dp);
+        if(dp[ind1][ind2]!=-1)
+        return dp[ind1][ind2];
+        if(str1[ind1-1]==str2[ind2-1])
+        return dp[ind1][ind2] = 1 + f(ind1-1,ind2-1,str1,str2,dp);
         else
-        return dp[ind1+1][ind2+1]= max(f(ind1-1,ind2,text1,text2,dp), f(ind1,ind2-1,text1,text2,dp));
+        return dp[ind1][ind2] = max(f(ind1,ind2-1,str1,str2,dp),f(ind1-1,ind2,str1,str2,dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int sz1 = text1.size();
-        int sz2 = text2.size();
-        vector<vector<int>>dp(sz1+1,vector<int>(sz2+1,0));
-        
-        for(int i=0;i<text2.size();i++){
-            dp[-1+1][i] = 0; 
-        }
-        for(int i=0;i<text1.size();i++){
-            dp[i][-1+1] = 0;
-        }
-        for(int ind1 = 1;ind1<=text1.size();ind1++){
-            for(int ind2 = 1; ind2<=text2.size(); ind2++){
-                if(text1[ind1-1]==text2[ind2-1])
-                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];// f(ind1-1, ind2-1,text1,text2,dp);
-                else dp[ind1][ind2]= max(dp[ind1][ind2-1], dp[ind1-1][ind2]);// f(ind1,ind2-1,text1,text2,dp));
-            }
-        }
-        return dp[sz1][sz2];
+        int s1 = text1.size();
+        int s2 = text2.size();
+        vector<vector<int>>dp(s1+1,vector<int>(s2+1,-1));
+        return f(s1,s2,text1,text2,dp);
     }
 };
