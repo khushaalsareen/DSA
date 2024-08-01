5f71e5fb747003b1a00c350c468be1b216b479dc
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-01 16:54:22 +0530

    Time: 105 ms (85.84%) | Memory: 12.4 MB (14.04%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -15,9 +15,21 @@ public:
     }
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        return f(1,n,nums,dp);
+        // return f(1,n,nums,dp);
+        for(int i = n;i>=1;i--){
+            for(int j=i;j<=n;j++){
+                int maxCoins = INT_MIN;
+        for(int k = i;k<=j;k++){
+            // last burst balloon is kth balloon
+            int coins = nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1] + dp[k+1][j];
+            maxCoins = max(maxCoins, coins);
+        }
+         dp[i][j]= maxCoins;
+            }
+        }
+        return dp[1][n];
     }
 };
