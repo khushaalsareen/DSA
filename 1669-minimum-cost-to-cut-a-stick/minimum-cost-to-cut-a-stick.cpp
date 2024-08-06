704a024c380413ffd5aa38e2dfc6809d9ef90f53
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-06 18:20:31 +0530

    Time: 36 ms (47.38%) | Memory: 12.5 MB (25.23%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
     int f(int i, int j, vector<int>&cuts,vector<vector<int>>&dp){
-        // there are no more cuts to consider
         if(i>j)
         return 0;
+
         if(dp[i][j]!=-1)
         return dp[i][j];
-        int minCost = INT_MAX;
-        for(int ind = i;ind<=j;ind++){
-            // make cut at index ind
-            int cost = cuts[j+1] - cuts[i-1] + f(i,ind-1,cuts,dp) + f(ind+1,j,cuts,dp);
-            minCost = min(cost,minCost);
-        }    
-        return dp[i][j] = minCost;
-    }
 
+        int ans =1e9;
+        for(int k=i;k<=j;k++){
+            int cost = cuts[j+1] - cuts[i-1] + f(i,k-1,cuts,dp) + f(k+1,j,cuts,dp);
+            ans = min(ans,cost);
+        }
+        return dp[i][j]= ans;
+    }
     int minCost(int n, vector<int>& cuts) {
         int c = cuts.size();
-        vector<vector<int>>dp(c+2,vector<int>(c+2,0));
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
-       
-        for(int i=c;i>=1;i--){
-            for(int j=i;j<=c;j++){
-                int minCost = INT_MAX;
-         for(int ind = i;ind<=j;ind++){
-            // make cut at index ind
-            int cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
-            minCost = min(cost,minCost);
-        }    
-        dp[i][j] = minCost;
-            }
-        }
-        return dp[1][c];
+        vector<vector<int>>dp(c+1,vector<int>(c+1,-1));
+        return f(1,c,cuts,dp);
     }
 };
