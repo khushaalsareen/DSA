4f9651ff09e4c4d81736600b22baeff178166abb
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-07 08:21:03 +0530

    Time: 1008 ms (5.86%) | Memory: 74.7 MB (25.77%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -18,7 +18,9 @@ public:
         int len = 0;
         int maxLen = 0;
         for(int i=0;i<nums.size();i++){
-            if(m[nums[i]]==true){
+            if(m.find(nums[i]-1)!=m.end())
+            m[nums[i]] = false;
+            else{
                 len = 1;
                 int ele = nums[i]+1;
                 while(m.find(ele)!=m.end()){
