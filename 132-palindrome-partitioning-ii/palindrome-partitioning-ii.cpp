a62ae13ce5ff63b82a1ac5eca9d9ec194fed9c6d
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-09 13:45:03 +0530

    Time: 1750 ms (21.30%) | Memory: 8.2 MB (48.69%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalindrome(int i, int j, string &s){
+        while(i<j){
+            if(s[i]!=s[j])
+            return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    int f(int ind, string &s, int n,vector<int>&dp){
+        if(ind == s.size())
+        return 0;
+        if(dp[ind]!=-1)
+        return dp[ind];
+        int minCost = 1e9;
+        for(int j = ind;j<n;j++){
+            if(isPalindrome(ind,j,s)){
+            int cost = 1 + f(j+1,s,n,dp);
+            minCost = min(cost,minCost);
+            }
+        }
+        return dp[ind]= minCost;
+    }
+    int minCut(string s) {
+        int n = s.size();
+        vector<int>dp(n,-1);
+        return f(0,s,n,dp)-1;
+    }
+};
