bbfabc0d4ecd2adb5a16d04d50001d1decd0f22d
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-11 03:26:58 +0530

    Time: 8 ms (73.87%) | Memory: 24.9 MB (5.14%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
-        if(n<=2)
+    int trap(vector<int>& heights) {
+        int n = heights.size();
+        if(n==2)
         return 0;
-        // vector<int>pmax(n);
-        vector<int>smax(n);
-       int pmax = height[0];
-        // for(int i=1;i<n;i++){
-        //     pmax[i] = max(height[i],pmax[i-1]);
-        // }
-        smax[n-1] = height[n-1];
-        for(int i=n-2;i>=0;i--){
-            smax[i] = max(height[i],smax[i+1]);
-        }
+        int lmax = heights[0];
+        int rmax = heights[n-1];
+        int l = 1;
+        int r = n-2;
         int cnt = 0;
-        for(int i=1;i<n-1;i++){
-            int dec_ht = min(pmax,smax[i+1]);
-            if(dec_ht<=height[i])
-            cnt+=0;
-            else
-            cnt+= dec_ht - height[i];
-            pmax = max(pmax,height[i]);
+        while(l<=r){
+            // left is deciding factor
+            if(lmax<=rmax){
+                if(lmax<=heights[l])
+                cnt+=0;
+                else
+                cnt+=lmax-heights[l];
+                lmax = max(lmax,heights[l]);
+                l++;
+            }
+            else{
+                // right is deciding factor
+                if(rmax<=heights[r])
+                cnt+=0;
+                else
+                cnt+=rmax-heights[r];
+                rmax = max(rmax,heights[r]);
+                r--;
+            }
         }
         return cnt;
     }
