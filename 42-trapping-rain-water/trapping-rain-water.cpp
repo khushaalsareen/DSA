5db507fd5ccf30e7f9cbc99360a47ee1b14c5966
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-11 02:26:22 +0530

    Time: 15 ms (21.22%) | Memory: 25.8 MB (5.14%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n =height.size();
-        int pmax[n],smax[n];
-        pmax[0] =height[0];
-        smax[n-1] =height[n-1];
+        int n = height.size();
+        if(n<=2)
+        return 0;
+        vector<int>pmax(n);
+        vector<int>smax(n);
+        pmax[0] = height[0];
         for(int i=1;i<n;i++){
-            pmax[i] =max(pmax[i-1],height[i]);
+            pmax[i] = max(height[i],pmax[i-1]);
         }
+        smax[n-1] = height[n-1];
         for(int i=n-2;i>=0;i--){
-            smax[i] =max(smax[i+1],height[i]);
+            smax[i] = max(height[i],smax[i+1]);
         }
-        int ans= 0;
-        for(int i=1;i<=n-2;i++){
-            int max_left_ht =pmax[i-1];
-            int max_right_ht=smax[i+1];
-            int curr_height= height[i];
-            int deciding_ht =min(max_left_ht,max_right_ht);
-            if(deciding_ht <= curr_height)
-            ans+=0;
+        int cnt = 0;
+        for(int i=1;i<n-1;i++){
+            int dec_ht = min(pmax[i-1],smax[i+1]);
+            if(dec_ht<=height[i])
+            cnt+=0;
             else
-            ans+= deciding_ht - curr_height;
+            cnt+= dec_ht - height[i];
         }
-        return ans;
+        return cnt;
     }
 };
