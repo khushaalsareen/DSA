18ac38ae44d6c5243965bb4c175ab804e82eed95
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-11 02:29:12 +0530

    Time: 11 ms (53.89%) | Memory: 25.4 MB (5.14%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -4,23 +4,24 @@ public:
         int n = height.size();
         if(n<=2)
         return 0;
-        vector<int>pmax(n);
+        // vector<int>pmax(n);
         vector<int>smax(n);
-        pmax[0] = height[0];
-        for(int i=1;i<n;i++){
-            pmax[i] = max(height[i],pmax[i-1]);
-        }
+       int pmax = height[0];
+        // for(int i=1;i<n;i++){
+        //     pmax[i] = max(height[i],pmax[i-1]);
+        // }
         smax[n-1] = height[n-1];
         for(int i=n-2;i>=0;i--){
             smax[i] = max(height[i],smax[i+1]);
         }
         int cnt = 0;
         for(int i=1;i<n-1;i++){
-            int dec_ht = min(pmax[i-1],smax[i+1]);
+            int dec_ht = min(pmax,smax[i+1]);
             if(dec_ht<=height[i])
             cnt+=0;
             else
             cnt+= dec_ht - height[i];
+            pmax = max(pmax,height[i]);
         }
         return cnt;
     }
