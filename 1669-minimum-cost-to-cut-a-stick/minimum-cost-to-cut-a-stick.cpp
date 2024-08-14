2a83bc64538df7de26849f74ed0d26fefdf828c5
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-15 00:13:52 +0530

    Time: 40 ms (30.74%) | Memory: 12.5 MB (30.00%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-    int f(int i, int j, vector<int>& cuts, vector<vector<int>>& dp) {
-        if (i > j)
-            return 0;
+    int f(int i, int j, vector<int>&cuts,vector<vector<int>>&dp){
+        if(i>j) return 0;
+        int minCost = 1e9;
+        
+        if(dp[i][j]!=-1)
+        return dp[i][j];
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int ans = 1e9;
-        for (int k = i; k <= j; k++) {
-            int cost = cuts[j + 1] - cuts[i - 1] + f(i, k - 1, cuts, dp) +
-                       f(k + 1, j, cuts, dp);
-            ans = min(ans, cost);
+        for(int k=i;k<=j;k++){
+            int cost = cuts[j+1] - cuts[i-1] + f(k+1,j,cuts,dp) + f(i,k-1,cuts,dp);
+            minCost = min(cost,minCost);
         }
-        return dp[i][j] = ans;
+        return dp[i][j]=minCost;
     }
     int minCost(int n, vector<int>& cuts) {
+        sort(cuts.begin(),cuts.end());
         int c = cuts.size();
         cuts.push_back(n);
-        cuts.insert(cuts.begin(), 0);
-        sort(cuts.begin(), cuts.end());
-        vector<vector<int>> dp(c + 2, vector<int>(c + 2, 0));
-        // return f(1,c,cuts,dp);
-        for (int i = c; i >= 1; i--) {
-            for (int j = 1; j <= c; j++) {
-                if(i>j)
-                continue;
-                int ans = 1e9;
-                for (int k = i; k <= j; k++) {
-                    int cost = cuts[j + 1] - cuts[i - 1] +
-                               dp[i][k-1] + dp[k+1][j];
-                    ans = min(ans, cost);
-                }
-                 dp[i][j] = ans;
-            }
-        }
-        return dp[1][c];
+        cuts.insert(cuts.begin(),0);
+        vector<vector<int>>dp(c+1,vector<int>(c+1,-1));
+        return f(1,c,cuts,dp);
     }
 };
