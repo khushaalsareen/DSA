5f0eda6143ca354b255c5801f6dc07c84da13d70
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-14 22:36:10 +0530

    Time: 44 ms (40.37%) | Memory: 47.7 MB (25.37%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
-    int f(int idx, int amount , vector<int>&coins,vector<vector<int>>&dp){
-        if(amount==0) return 0;
-        if(idx==0){
-            if(amount%coins[0] == 0)
-            return amount/coins[0];
-            else
+    int f(int ind, int target,vector<int>&coins,vector<vector<int>>&dp){
+        if(ind==0){
+            if(target%coins[0] == 0)
+            return target/coins[0];
             return 1e9;
         }
-        if(dp[idx][amount]!=-1) return dp[idx][amount];
-        int notpick = 0 + f(idx-1,amount,coins,dp);
-        int pick = INT_MAX;
-        if(coins[idx]<=amount)
-        pick = 1 + f(idx,amount-coins[idx],coins,dp);
-
-        return dp[idx][amount] = min(pick,notpick);
+        if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        int notpick = f(ind-1,target,coins,dp);
+        int take = 1e9;
+        if(coins[ind]<=target)
+        take = 1 + f(ind,target-coins[ind],coins,dp);
+        return dp[ind][target] = min(take,notpick);
     }
-    int coinChange(vector<int>& coins, int Amount) {
-        int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(Amount+1,-1));
-        // int ans = f(n-1,amount,coins,dp);
-        // return ans>=1e9?-1:ans;
-
-        for(int i=0;i<n;i++){
-            dp[i][0] = 0;
-        }
-        for(int i =0;i<=Amount;i++){
-            dp[0][i] = i%coins[0] == 0? i/coins[0] : 1e9;
-        }
-
-        for(int idx =1;idx<n;idx++){
-            for(int amount = 1;amount<=Amount;amount++){
-                int notpick = 0 + dp[idx-1][amount];// f(idx-1,amount,coins,dp);
-                int pick = INT_MAX;
-                if(coins[idx]<=amount)
-                pick = 1 + dp[idx][amount-coins[idx]];// f(idx,amount-coins[idx],coins,dp);
-
-                dp[idx][amount] = min(pick,notpick);
-            }
-        }
-        return dp[n-1][Amount]>=1e9 ? -1 : dp[n-1][Amount];
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+        int ans =  f(coins.size()-1,amount,coins,dp);
+        if(ans>=1e9)
+        return -1;
+        return ans;
     }
 };
