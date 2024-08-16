b42318eb61ca2ea5b96a9133f75fcf21a5e5e102
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-16 15:41:35 +0530

    Time: 25 ms (8.21%) | Memory: 21.5 MB (13.39%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/wiggle-sort-ii.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    static bool cmp(int x, int y){
-        return x>y;
-    }
     void wiggleSort(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1)
+        if(nums.size()==1)
         return;
-        sort(nums.begin(),nums.end(),cmp);
-        vector<int> ans;
-        ans = nums;
-        int j = 0;
-        for(int i=1;i<n;i+=2){
-            nums[i] = ans[j];
-            j++;
+        priority_queue<int>pq(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i+=2){
+            nums[i] = pq.top();
+            pq.pop();
         }
-        
-        for(int i=0;i<n;i+=2){
-            nums[i] = ans[j];
-            j++;
+        for(int i=0;i<nums.size();i+=2){
+            nums[i] = pq.top();
+            pq.pop();
         }
-        
     }
 };
