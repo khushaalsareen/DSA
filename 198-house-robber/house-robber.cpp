1cba4cc5d22c61dfa5b912d757c651fc8a551dbd
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-27 22:55:41 +0530

    Time: 2 ms (39.44%) | Memory: 9.5 MB (50.04%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
-    int f(int idx, vector<int>&nums,vector<int>&dp){
-        if(idx==0)
-          return nums[0];
-        if(idx<0) 
-          return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int take = f(idx-2,nums,dp) + nums[idx];
-        int notTake = f(idx-1,nums,dp) + 0;
-        return dp[idx] = max(take,notTake);
+    int f(int ind, vector<int>&nums,vector<int>&dp){
+        if(ind == 0)
+        return nums[0];
+        if(ind < 0)
+        return 0;
+        if(dp[ind]!=-1)
+        return dp[ind];
+        int pick = nums[ind] + f(ind-2,nums,dp);
+        int notPick = f(ind-1,nums,dp);
+        return dp[ind] = max(pick,notPick);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        // vector<int>dp(n,0);
-        // return f(nums.size()-1,nums,dp);
-        int prev1 = nums[0];
-        int prev2 = 0;
-        for(int idx=1;idx<n;idx++){
-        int take = nums[idx];
-        if(idx>=2)
-        take += prev2;
-        int notTake = prev1 + 0;
-        int curr = max(take,notTake);
-        prev2 = prev1;
-        prev1 = curr;
-        }
-        return prev1;
+        vector<int>dp(n,-1);
+        return f(n-1,nums,dp);
     }
 };
