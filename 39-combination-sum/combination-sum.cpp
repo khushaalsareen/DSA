5035835112ac00a3e0e74b3552e29ef57e501764
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-30 02:03:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (56.19%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    void generate(int ind,vector<int>&v, vector<int>& candidates, int target, vector<vector<int>> &ans ){
-        if(target == 0)
-        {
+    void generate(int ind, int target, vector<int>&v, vector<vector<int>>&ans, vector<int>&candidates){
+        if(target == 0){
             ans.push_back(v);
             return;
         }
         if(ind == candidates.size()){
-            if(target == 0)
-            ans.push_back(v);
             return;
         }
-       
-        if(candidates[ind]<=target)
-       { v.push_back(candidates[ind]);
-        generate(ind,v,candidates,target-candidates[ind],ans);
-        v.pop_back();}
-
-         generate(ind+1,v,candidates,target,ans);
+        if(candidates[ind]<=target){
+        v.push_back(candidates[ind]);
+        generate(ind,target-candidates[ind],v,ans,candidates);
+        v.pop_back();
+        }
+        int j=ind+1;
+        for( ;j<candidates.size();j++){
+            if(candidates[j]!=candidates[j-1])
+            break;
+        }
+        generate(j,target,v,ans,candidates);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int>v;
-        generate(0,v,candidates,target,ans);
+        sort(candidates.begin(),candidates.end());
+        generate(0,target,v,ans,candidates);
         return ans;
     }
 };
