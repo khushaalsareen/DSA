100ef549326998ffeaf1278e0f11a91ccb2bcae7
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-29 11:10:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (39.66%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
-   int lastStoneWeightII(vector<int>& stones) {
-    int totalSum = accumulate(stones.begin(), stones.end(), 0);
-    int target = totalSum / 2;
-    
-    vector<int> dp(target + 1, 0);
-    
-    for (int stone : stones) {
-        for (int i = target; i >= stone; --i) {
-            dp[i] = max(dp[i], dp[i - stone] + stone);
+    bool f(int ind, int target, vector<int>&nums,vector<vector<int>>&dp){
+        if(target == 0)
+        return true;
+        if(ind == 0){
+            return target == nums[ind];
         }
+        if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        bool nottake = f(ind-1,target,nums,dp);
+        bool take = false;
+        if(nums[ind]<=target)
+        take = f(ind-1,target-nums[ind],nums,dp);
+        return dp[ind][target] = take || nottake;
     }
-    
-    return totalSum - 2 * dp[target];
-}
-
+    int lastStoneWeightII(vector<int>& stones) {
+        int n = stones.size();
+        int sum = accumulate(stones.begin(),stones.end(),0);
+        int target = sum/2;
+        vector<vector<int>>dp(n,vector<int>(target+1,false));
+        // return f(n-1,target,stones,dp);
+        vector<int>nums = stones;
+        if(nums[0]<=target)
+        dp[0][nums[0]] = true;
+        for(int i=0;i<n;i++){
+            dp[i][0] = true;
+        }
+        for(int ind = 1;ind<n;ind++){
+            for(int target = 1;target<=sum/2;target++){
+                bool nottake = dp[ind-1][target];
+            bool take = false;
+            if(nums[ind]<=target)
+            take = dp[ind-1][target-nums[ind]];// f(ind-1,target-nums[ind],nums,dp);
+             dp[ind][target] = take || nottake;
+            }
+        }
+        for(int i=sum/2;i>=0;i--){
+            if(dp[n-1][i])
+            return sum - (2 * i);
+        }
+        return 0;
+    }
 };
