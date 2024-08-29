2dfc24076fe732338e46ee4f912ac6382d7329db
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-08-30 00:43:03 +0530

    Time: 16 ms (88.03%) | Memory: 29.5 MB (77.78%) - LeetSync

--- 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
@@ -2,46 +2,24 @@ class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
-        stack<int>s;
-        s.push(0);
-        // next smaller element
-        vector<int>nse(n,n);
-        for(int i=1;i<n;i++){
-            while(!s.empty() && nums[i]<nums[s.top()]){
-                nse[s.top()] = i;
-                s.pop();
-            }
-            s.push(i);
-        }
-        while(!s.empty()){
-            s.pop();
-        }
-        // previous gretaer
-        s.push(n-1);
-        vector<int>pge(n,-1);
+        int currMin = nums[n-1];
+        int start = n-1;
         for(int i=n-2;i>=0;i--){
-            while(!s.empty() && nums[i]>nums[s.top()]){
-                pge[s.top()] = i;
-                s.pop();
-            }
-            s.push(i);
+            if(nums[i]>currMin)
+            start = i;
+            else
+            currMin = nums[i];
         }
-        int st = n;
-        int end = n-1;
-        for(int i=0;i<n;i++){
-            if(nse[i]!=n){
-                st = i;
-                break;
-            }
+        int end=  0;
+        int currMax = nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]>=currMax)
+            currMax = nums[i];
+            else
+            end = i;
         }
-        if(st == n)
+        if(start == n-1)
         return 0;
-        for(int i=n-1;i>=0;i--){
-            if(pge[i]!=-1){
-                end = i;
-                break;
-            }
-        }
-        return end - st + 1;
+        return end - start + 1;
     }
 };
