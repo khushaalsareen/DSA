230ce4e06938dcc51c42b345d8fdb8cad0045062
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-04 00:03:14 +0530

    Time: 13 ms (38.54%) | Memory: 18.9 MB (92.34%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -11,34 +11,31 @@
  */
 class Solution {
 public:
-    void generate(TreeNode*root,int target, vector<int>&v,vector<vector<int>>&ans){
+     void generate( vector<vector<int>>&ans, vector<int>&v, TreeNode*root, int target, int &sum){
         if(!root)
         return;
-        // if(root->val > target) return;
+        
+        v.push_back(root->val);
+        sum+=root->val;
         if(!root->left && !root->right){
-            if(root->val == target)
-            {
-            v.push_back(root->val);
+            if(sum == target)
             ans.push_back(v);
             v.pop_back();
-           
-            }
+            sum-=root->val;
             return;
         }
-
-        v.push_back(root->val);
-        // cout<<"Yes"<<" "<<root->val<<" ";
-        generate(root->left,target-root->val,v,ans);
-        generate(root->right,target-root->val,v,ans);
+        
+        generate(ans,v,root->left,target,sum);
+        generate(ans,v,root->right,target,sum);
+        
         v.pop_back();
-
+        sum-=root->val;
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>>ans;
-        if(!root)
-        return ans;
-        vector<int>v;
-        generate(root,targetSum,v,ans);
-        return ans;
+         vector<int>v;
+         int sum = 0;
+         generate(ans,v,root,targetSum,sum);
+         return ans;
     }
 };
