3a0e62fb6609183adbf1cacb6835a8db4a4ef93d
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-05 17:02:51 +0530

    Time: 3 ms (85.77%) | Memory: 16.6 MB (40.01%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -8,37 +8,35 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// 1   k = 5
+
 class Solution {
 public:
-    int len(ListNode*head){
-        int cnt = 0;
-        ListNode*temp = head;
-        while(temp){
-            cnt++;
-            temp = temp->next;
-        }
-        return cnt;
-    }
     ListNode* rotateRight(ListNode* head, int k) {
         if(!head)
         return NULL;
-        int length = len(head);
-        k = k%length;
-        if(k==0)
-        return head;
-        // cout<<length<<endl;
+        int len = 0;
         ListNode*temp = head;
-        for(int i = 0;i<length-k-1;i++){
-            temp = temp->next;  
+        ListNode*tail = NULL;
+        while(temp){
+            tail = temp;
+            len++;
+            temp = temp->next;
         }
-        ListNode*nextNode = temp->next;
-        ListNode* newHead = nextNode;
+        k  = k%len;
+        if(k == 0)
+        return head;
+        int cnt = 0;
+        temp = head;
+        while(cnt<len - k - 1){
+            temp = temp->next;
+            cnt++;
+        }
+        ListNode* sp = temp->next;
         temp->next = NULL;
-        while(nextNode && nextNode->next!=NULL){
-            nextNode = nextNode->next;
-        }
-        nextNode->next = head;
-        head = nextNode;
-        return newHead;
+        tail->next = head;
+        return sp;
+
     }
 };
