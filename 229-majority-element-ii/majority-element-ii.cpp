af0dba48253561d2aa62b9ec7331ee2373b9551a
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-06 23:25:01 +0530

    Time: 11 ms (41.11%) | Memory: 18.7 MB (73.91%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        int maj1 = -1e9-1;
-        int maj2 = -1e9-1;
-        int freq1 = 0;
-        int freq2 = 0;
-        int idx = 0;
-        while(idx<nums.size()){
-            if(maj1==nums[idx   ])
-            freq1++;
-            else if(maj2==nums[idx])
-            freq2++;
-            else if(freq1 == 0){
-                maj1 = nums[idx];
-                freq1 = 1;
+        vector<int>ans;
+        int num1 = -1e9;
+        int num2 = -1e9;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==num1)
+            cnt1++;
+            else if(nums[i]==num2)
+            cnt2++;
+            else if(cnt1==0){
+                num1 = nums[i];
+                cnt1++;
             }
-            else if(freq2 == 0){
-                maj2 = nums[idx];
-                freq2 = 1;
+            else if(cnt2==0){
+                num2 = nums[i];
+                cnt2++;
             }
             else{
-                freq1--;
-                freq2--;
+                cnt1--;
+                cnt2--;
             }
-            idx++;
         }
-        int check1 =0;
-        int check2 = 0;
+        int f1 = 0;
+        int f2 = 0;
         for(auto it:nums){
-            if(it==maj1)
-            check1++;
-            if(it==maj2)
-            check2++;
+            if(it==num1)
+            f1++;
+            if(it==num2)
+            f2++;
         }
-        vector<int>ans;
-        if(check1>n/3)
-        ans.push_back(maj1);
-        if(check2>n/3)
-        ans.push_back(maj2);
+        int n = nums.size();
+        if(f1<=n/3 && f2<=n/3)
+        return {};
+        if(f1>n/3)
+        ans.push_back(num1);
+        if(f2>n/3)
+        ans.push_back(num2);
         return ans;
     }
 };
