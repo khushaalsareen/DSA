c12a079a692269eab74461bfaec0aebf704d9658
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-07 03:11:09 +0530

    Time: 166 ms (39.23%) | Memory: 29.1 MB (17.81%) - LeetSync

--- 454-4sum-ii/4sum-ii.cpp
+++ 454-4sum-ii/4sum-ii.cpp
@@ -7,23 +7,18 @@ public:
         int n3 = nums3.size();
         int n4 = nums4.size();
         unordered_map<long long int,int>m1;
-        unordered_map<long long int,int>m2;
+        // unordered_map<long long int,int>m2;
         for(int i=0;i<n1;i++){
             for(int j=0;j<n2;j++){
                 m1[nums1[i]+nums2[j]]++;
             }
         }
+        int ans = 0;
         for(int i=0;i<n3;i++){
             for(int j=0;j<n4;j++){
-                m2[nums3[i]+nums4[j]]++;
-            }
-        }
-        int ans = 0;
-        for(auto it:m1){
-            // it is pair
-            int target = -1*it.first;
-            if(m2.find(target)!=m2.end()){
-                ans+=(it.second*m2[target]);
+                if(m1.find(-1*(nums3[i]+nums4[j]))!=m1.end()){
+                    ans+=m1[-1*(nums3[i]+nums4[j])];
+                }
             }
         }
         return ans;
