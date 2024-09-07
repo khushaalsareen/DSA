cdcb9f7936617763d16182c861e6eff17099eac7
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-08 03:56:40 +0530

    Time: 290 ms (24.65%) | Memory: 9.5 MB (72.70%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
-    string expand(string &s, int i, int j){
-        while(i>=0 && j<s.size() && s[i]==s[j]){
-            i--;
-            j++;
+    int findLength(string &nums1, string &nums2, int &ansI, int &ansJ) {
+    int s1 = nums1.size();
+    int s2 = nums2.size();
+    int ans = 0;
+
+    vector<int> prev(s2 + 1, 0);
+    vector<int> curr(s2 + 1, 0);
+
+    for (int i = 1; i <= s1; i++) {
+        for (int j = 1; j <= s2; j++) {
+            if (nums1[i - 1] == nums2[j - 1]) {
+                curr[j] = 1 + prev[j - 1]; // f(i-1,j-1,nums1,nums2);
+            } else {
+                curr[j] = 0;
+            }
+
+            // Update the answer and check if the indices match the palindromic condition
+            if (curr[j] > ans) {
+                int startInOriginal = i - curr[j];
+                int startInReversed = s2 - j; // Corresponding index in the reversed string
+
+                // Only update if it's a valid palindrome
+                if (startInOriginal == startInReversed) {
+                    ansI = i;
+                    ansJ = j;
+                    ans = curr[j];
+                }
+            }
         }
-        return s.substr(i+1,j-i-1);
+        prev = curr;
     }
-    string longestPalindrome(string s) {
-        int n = s.size();
-        string maxi="";
-        for(int i =0;i<n;i++){
-            string even = "";
-            string odd = expand(s,i,i);
-            if(i+1<n)
-             even = expand(s,i,i+1);
-            if(odd.size()>maxi.size())
-            maxi = odd;
-            if(even.size()>maxi.size())
-            maxi = even;
-        }
-        return maxi;
-    }
+
+    return ans;
+}
+
+string longestPalindrome(string s) {
+    string reversed = s;
+    reverse(reversed.begin(), reversed.end());
+
+    int ansI = -1, ansJ = -1;
+    int len = findLength(s, reversed, ansI, ansJ);
+
+    // Extract the substring
+    return s.substr(ansI - len, len);
+}
+
 };
