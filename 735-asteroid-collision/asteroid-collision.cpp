f103f99e322b05e2557ae925a41cf9cbdc8642fc
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-09 16:28:50 +0530

    Time: 13 ms (28.91%) | Memory: 20.8 MB (66.03%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& arr) {
-        vector<int>ans;
-        stack<int>s;
-        s.push(0);
-        for(int i=1;i<arr.size();i++){
-            if(arr[i]>0)
-            s.push(i);
-            //collsiion will occur
-            bool survived = true;
-            while(!s.empty() && arr[i]<0 && arr[s.top()]>0){
-                int x = s.top(); // index of last element on top of stack
-                s.pop();
-                if(abs(arr[x])>abs(arr[i])){
-                    s.push(x);
-                    survived = false;
-                    break;
+    vector<int> asteroidCollision(vector<int>& nums) {
+            int n  = nums.size();
+            stack<int>s;
+            s.push(nums[0]);
+            for(int i=1;i<n;){
+                if(!s.empty() && (s.top()>0 && nums[i]<0)){
+                    if(abs(s.top())>abs(nums[i])){
+                        i++;
+                    }
+                    else if(abs(s.top())==abs(nums[i])){
+                        s.pop();
+                        i++;
+                    }
+                    else{
+                        s.pop();
+                    }
                 }
-                else if(abs(arr[x])==abs(arr[i]))
-                {
-                    survived = false;
-                    break;
+                else{
+                    s.push(nums[i]);
+                    i++;
                 }
-                else
-                continue; // continue knocking out elements from stack
             }
-            if(survived && (s.empty() || arr[s.top()]<0))
-            s.push(i);   
-        }
-        while(!s.empty()){
-            ans.push_back(arr[s.top()]);
-            s.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+            vector<int>ans;
+            while(!s.empty()){
+                ans.push_back(s.top());
+                s.pop();
+            }
+            reverse(ans.begin(),ans.end());
+            return ans;
     }
 };
