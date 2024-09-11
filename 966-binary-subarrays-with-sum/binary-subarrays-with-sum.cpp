e4517fb80e1a5e063ab358c2e30bd408d3f01854
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-12 02:34:38 +0530

    Time: 46 ms (20.87%) | Memory: 38.6 MB (19.26%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,41 +1,18 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-    int n = nums.size();
-    long long sum = 0;
-    int i = 0, j = 0;
-    long long cnt1 = 0, cnt2 = 0;
-
-    // First, count subarrays with sum exactly equal to the goal
-    while (j < n) {
-        sum += nums[j];
-        while (sum > goal) {
-            sum -= nums[i];
-            i++;
+        int n = nums.size();
+        unordered_map<int,int>m;
+        m.insert({0,1});
+        int psum = 0;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            psum+=nums[i];
+            if(m.find(psum-goal)!=m.end()){
+                cnt+=m[psum-goal];
+            }
+            m[psum]++;
         }
-        cnt1 += (j - i + 1);
-        j++;
+        return cnt;
     }
-    if(goal == 0)
-    return cnt1;
-    // Reset for the next loop
-    i = 0;
-    j = 0;
-    sum = 0;
-    int newT = goal - 1;
-
-    // Now, count subarrays with sum equal to goal - 1
-    while (j < n) {
-        sum += nums[j];
-        while (sum > newT) {
-            sum -= nums[i];
-            i++;
-        }
-        cnt2 += (j - i + 1);
-        j++;
-    }
-
-    // Return the difference
-    return cnt1 - cnt2;
-}
 };
