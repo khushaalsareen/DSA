0bd18fa330ef0268a17e7d28754db4133f4b4e1c
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-18 16:37:41 +0530

    Time: 3 ms (72.41%) | Memory: 14.1 MB (82.95%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -10,66 +10,51 @@
  * };
  */
 class Solution {
+      static  bool cmp(pair<int,TreeNode*>&p1,pair<int,TreeNode*>&p2){
+        if(p1.first == p2.first)
+        return p1.second->val < p2.second->val;
+        return p1.first < p2.first;
+      }
 public:
- 
-static bool cmp(pair<int,int>p1, pair<int,int>p2){
-    if(p1.second == p2.second)
-    return p1.first<p2.first;
-    return p1.second<p2.second;
-}
-void levelOrder(TreeNode* root, vector<vector<int>>& ans, unordered_map<int, vector<pair<int,int>>>& m, int& mini, int& maxi) {
-    queue<pair<TreeNode*, int>> q;
-    q.push({root, 0});
-    int prevVert = -1;
-    int level = 0;
-    while (!q.empty()) {
-        int sz = q.size();
-        for(int i=0;i<sz;i++){
-        pair<TreeNode*, int> p = q.front();
-        q.pop();
-        TreeNode* currNode = p.first;
-        int currVert = p.second;
-        
-        mini = min(mini, currVert);
-        maxi = max(maxi, currVert);
-        
-        m[currVert].push_back({currNode->val,level});
-
-        if (currNode->left) {
-            q.push({currNode->left, currVert - 1});
-        }
-
-        if (currNode->right) {
-            q.push({currNode->right, currVert + 1});
+    // assigning horizonatl and vertical
+    void levelOrder(TreeNode*root,unordered_map<int,vector<pair<int,TreeNode*>>>&m,int &mini, int &maxi){
+        queue<pair<TreeNode*,pair<int,int>>>q;
+        q.push({root,{0,0}});
+        while(!q.empty()){
+            pair<TreeNode*,pair<int,int>> p = q.front();
+            q.pop();
+            TreeNode* currNode = p.first;
+            int currVert = p.second.first;
+            int currHori = p.second.second;
+            mini = min(mini,currVert);
+            maxi = max(maxi,currVert);
+            m[currVert].push_back({currHori,currNode});
+            if(currNode->left)
+            q.push({currNode->left,{currVert-1,currHori+1}});
+            if(currNode->right)
+            q.push({currNode->right,{currVert+1,currHori+1}});
         }
     }
-        level++;
-        
-    }
-}
-
-vector<vector<int>> verticalTraversal(TreeNode* root) {
-    vector<vector<int>>ans;
-    if (!root) {
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> ans;
+        unordered_map<int,vector<pair<int,TreeNode*>>>m;
+        int mini = 1e9;
+        int maxi = -1e9;
+        levelOrder(root,m,mini,maxi);
+        for(auto &it:m){
+            cout<<"vertical "<<it.first<<" ";
+            for(auto j:it.second){
+                cout<<"Horizonatl "<<j.first<<" "<<"Value "<<j.second->val<<endl;
+            }
+            sort(it.second.begin(),it.second.end(),cmp);
+        }
+        for(int i=mini;i<=maxi;i++){
+            vector<int>v;
+            for(auto it:m[i]){
+                v.push_back(it.second->val);
+            }
+            ans.push_back(v);
+        }
         return ans;
     }
-
-    unordered_map<int, vector<pair<int,int>>> m;
-    int maxi = -1;
-    int mini = 1e9;
-    
-    levelOrder(root, ans, m, mini, maxi);
-    
-    for (int i = mini; i <= maxi; i++) {
-        sort(m[i].begin(),m[i].end(),cmp);
-        int sz = m[i].size();
-        vector<int>v;
-       for(auto it:m[i]){
-        v.push_back(it.first);
-       }
-       ans.push_back(v);
-    }
-    
-    return ans;
-}
 };
