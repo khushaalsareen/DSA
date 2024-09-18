8ba4dbce20e637ec63cbf49852da9cde370813b5
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-19 00:19:53 +0530

    Time: 17 ms (6.85%) | Memory: 15 MB (11.39%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& grid,vector<vector<int>>&dp){
+    int f(int i, int j,vector<vector<int>>& grid,vector<vector<int>>&dp){
         if(i==0 && j==0)
         return grid[0][0];
-        if(i<0 || j<0)
-        return 1e9;
+        if(i<0 ||  j<0)
+        return 1e9; // invalid  path
         if(dp[i][j]!=-1)
         return dp[i][j];
-
-        int m = grid.size();
-        int n = grid[0].size();
-
         int left = grid[i][j] + f(i,j-1,grid,dp);
         int up = grid[i][j] + f(i-1,j,grid,dp);
-
         return dp[i][j] = min(left,up);
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,grid,dp);
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,grid,dp);
     }
 };
