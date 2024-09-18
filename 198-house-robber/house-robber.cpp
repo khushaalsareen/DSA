03fa8fe41b1c67bd37953e4538d92429058fad6e
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-18 23:42:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (47.87%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -13,7 +13,17 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,-1);
-        return f(n-1,nums,dp);
+        vector<int>dp(n,0);
+        dp[0] = nums[0];
+        if(n==1)
+        return dp[0];
+        dp[1] = max(nums[0],nums[1]);
+        // return f(n-1,nums,dp);
+        for(int ind=2;ind<n;ind++){
+            int notPick = dp[ind-1];
+            int pick = nums[ind] + dp[ind-2] ;
+          dp[ind] = max(pick,notPick);
+        }
+        return dp[n-1];
     }
 };
