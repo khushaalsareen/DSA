120f000c3ebaeccc4820a677b4b4ffde913738e6
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-21 17:55:02 +0530

    Time: 26 ms (38.85%) | Memory: 29.6 MB (47.23%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,51 +1,33 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-  
-        bool flag = false;
-
-        if(nums[0]>=0)
-        flag = true;
-
-        int idx = -1;
-        for(int i=0;i<n;i++){
-            if(nums[i]>=0){
-                idx = i;
-                break;
-            }
+        vector<int> answer;
+        int n=nums.size();
+        int i=0;
+        int j;
+        for(;i<n;i++){
+            if(nums[i]>=0)
+            break;
         }
-        for(int i=0;i<n;i++){
-            nums[i] = nums[i]*nums[i];
-        }
-        if(flag)
-        return nums;
-        
-        int j = idx - 1;
-        vector<int>ans;
-        if(idx == -1){
-            reverse(nums.begin(),nums.end());
-            return nums;
-        }
-
-        while(j>=0 && idx<n){
-            if(nums[idx]>nums[j]){
-                ans.push_back(nums[j]);
-                j--;
+        j=i-1;
+        while(j>=0 && i<n){
+            if(nums[i]*nums[i]<=nums[j]*nums[j]){
+                answer.push_back(nums[i]*nums[i]);
+                i++;
             }
             else{
-                ans.push_back(nums[idx]);
-                idx++;
+                answer.push_back(nums[j]*nums[j]);
+                j--;
             }
         }
-        while( idx<n){
-            ans.push_back(nums[idx]);
-                idx++;
+        while(i<n){
+            answer.push_back(nums[i]*nums[i]);
+                i++;
         }
         while(j>=0){
-            ans.push_back(nums[j]);
+            answer.push_back(nums[j]*nums[j]);
                 j--;
         }
-        return ans;
+        return answer;
     }
 };
