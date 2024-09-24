3fe1f3ff29bea68dc95affc76c75766d8cc5db86
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-24 11:57:54 +0530

    Time: 16 ms (70.49%) | Memory: 12 MB (67.92%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     pair<int,pair<int,int>> f(string &s, int i, int j){
-        int len = 1;
+        int len = i+1==j?0:1;
         while(i>=0 && j<s.size() && s[i]==s[j]){
             len+=2;
             i--;
@@ -9,15 +9,7 @@ public:
         }
         return {len,{i+1,j-1}};
     }
-    pair<int,pair<int,int>> e(string &s, int i, int j){
-        int len = 0;
-        while(i>=0 && j<s.size() && s[i]==s[j]){
-            len+=2;
-            i--;
-            j++;
-        }
-        return {len,{i+1,j-1}};
-    }
+    
 
     string longestPalindrome(string s) {
         int n = s.size();
@@ -27,7 +19,7 @@ public:
         for(int i=0;i<n;i++){
             // expand around centre i
             pair<int,pair<int,int>> oddlen = f(s,i-1,i+1);
-            pair<int,pair<int,int>> evenlen = e(s,i-1,i);
+            pair<int,pair<int,int>> evenlen = f(s,i-1,i);
             if(oddlen.first>evenlen.first)
             {
                 if(oddlen.first>ans.size()){
