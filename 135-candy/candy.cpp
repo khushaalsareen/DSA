8dda988c5374a1273a4fac745ee3c53e7ea30243
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-09-28 00:27:35 +0530

    Time: 15 ms (55.67%) | Memory: 19.8 MB (81.94%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,51 +1,32 @@
 class Solution {
 public:
-    int candy(vector<int>& nums) {
-        int n = nums.size();
-        int mini = *min_element(nums.begin(),nums.end());
-        int maxi = *max_element(nums.begin(),nums.end());
-        if(mini == maxi)
-        return n;
-        vector<int>v(n);
-        queue<pair<int,int>>q;
-        if(nums[0]<=nums[1])
-        q.push({0,1});
-        if(nums[n-1]<=nums[n-2])
-        q.push({n-1,1});
-        for(int i=1;i<n-1;i++){
-            // if(nums[i]<nums[i-1] && nums[i]<nums[i+1])
-            // q.push({i,1});
-            // else if(nums[i]<nums[i-1] && nums[i]==nums[i+1])
-            // q.push({i,1});
-            // else if(nums[i]==nums[i-1] && nums[i]==nums[i+1])
-            // q.push({i,1});
-            // else if(nums[i]==nums[i-1] && nums[i]<nums[i+1])
-            // q.push({i,1});
-            // else if(nums[i]==nums[i+1] && nums[i]<nums[i-1])
-            // q.push({i,1});
-            if((nums[i]>nums[i-1] && nums[i]<nums[i+1])|| (nums[i]>nums[i+1] && nums[i]<nums[i-1]))
-            continue;
-            else
-            q.push({i,1});
-        }
-        while(!q.empty()){
-            pair<int,int> p = q.front();
-            q.pop();
-            int currIdx = p.first;
-            int currCandy = p.second;
-            v[currIdx] = currCandy;
-            if(currIdx>0 && nums[currIdx]<nums[currIdx-1]){
-                q.push({currIdx-1,currCandy+1});
-            }
-            if(currIdx<n-1 && nums[currIdx]<nums[currIdx+1]){
-                q.push({currIdx+1,currCandy+1});
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int candy = n, i=1;
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                i++;
+                continue;
             }
             
+            //For increasing slope
+            int peak = 0;
+            while(ratings[i] > ratings [i-1]){
+                peak++;
+                candy += peak;
+                i++;
+                if(i == n) return candy;
+            }
+            
+            //For decreasing slope
+            int valley = 0;
+            while(i<n && ratings[i] < ratings[i-1]){
+                valley++;
+                candy += valley;
+                i++;
+            }
+            candy -= min(peak, valley); //Keep only the higher peak
         }
-        int sum = 0;
-        for(auto it:v){
-            sum+=it;
-        }
-        return sum;
+        return candy;
     }
 };
