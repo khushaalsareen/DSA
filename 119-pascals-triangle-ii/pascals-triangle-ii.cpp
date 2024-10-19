4399b1f32a04b87b006c0fd31847f9d56dceae95
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-10-19 18:17:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (51.71%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> getRow(int r) {
-        vector<vector<int>>dp;
-        dp.push_back({1});
+        // vector<vector<int>>dp;
+        vector<int>prev;
+        prev.push_back(1);
         for(int i=1;i<=r;i++){
             vector<int>v(i+1);
             v[0] = 1;
             v[i] = 1;
             for(int j=1;j<i;j++){
-                v[j] = dp[i-1][j] + dp[i-1][j-1];
+                v[j] = prev[j] + prev[j-1];
             }
-            dp.push_back(v);
+            prev = v;
         }
-        return dp[r];
+        return prev;
     }
 };
