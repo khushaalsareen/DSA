9d473cafac481d54d953af4b337f2b4e511fb744
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-10-25 14:17:50 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (41.82%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<vector<int>>s;
+        int n = nums.size();
+        int ts = 1<<n;
+        for(int mask = 0; mask<ts;mask++){
+            vector<int>v;
+            for(int bitPos = 0;bitPos<n;bitPos++){
+                if(mask & (1<<bitPos)){
+                    v.push_back(nums[bitPos]);
+                }
+            }
+            if(s.find(v)==s.end()){
+                ans.push_back(v);
+                s.insert(v);
+            }
+        }
+        return ans;
+    }
+};
