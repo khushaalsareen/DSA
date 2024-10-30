a4e29f0e09e96d034f505a100ff660b18164b333
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-10-30 15:24:27 +0530

    Time: 3 ms (71.39%) | Memory: 9.7 MB (64.17%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    void find(map<long long int,int>&m, int n, int &ans,auto &itr, int &cnt){
-       
-        cnt++;
-        if(cnt==n){
-            ans = itr->first;
-            return;
-        }
-        long long int val1 = itr->first*1LL*2;
-        long long int val2 = itr->first*1LL*3;
-        long long int val3 = itr->first*1LL*5;
-        m[val1]++;
-        m[val2]++;
-        m[val3]++;
-        itr++;
-        find(m,n,ans,itr,cnt);
-    }
     int nthUglyNumber(int n) {
-        map<long long int, int>m;
-        m[1*1LL]++;
-        int ans = 1;
-        auto itr = m.begin();
-        int cnt=0;
-        find(m,n,ans,itr,cnt);
-        return ans;
+        vector<int>dp(n);
+        dp[0] = 1;
+        int p2 = 0;
+        int p3 = 0;
+        int p5 = 0;
+        for(int i=1;i<n;i++){
+            dp[i] = min({dp[p2]*2,dp[p3]*3,dp[p5]*5});
+            if(dp[i]==dp[p2]*2)
+            p2++;
+            if(dp[i]==dp[p3]*3)
+            p3++;
+            if(dp[i]==dp[p5]*5)
+            p5++;
+        }
+        return dp[n-1];
     }
 };
