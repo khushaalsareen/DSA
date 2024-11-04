3123aa89465045be20673904e32c328840144de2
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-11-05 00:07:15 +0530

    Time: 1 ms (44.38%) | Memory: 16.7 MB (23.90%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
-    int bfs(vector<vector<int>>&visited , vector<vector<int>>&grid,vector<pair<int,int>> &directions){
-        int m = grid.size();
-        int ans = 0;
-        int n = grid[0].size();
-        queue<pair<int,pair<int,int>>>q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==2)
-                {q.push({0,{i,j}});
-                visited[i][j] = 1;
+    int bfs( vector<vector<int>>&visited, vector<pair<int,int>>&directions,vector<vector<int>>&grid){
+        queue<pair<pair<int,int>,int>>q;
+        
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    visited[i][j] = 1;
                 }
             }
         }
+        int cnt = 0;
         while(!q.empty()){
-            pair<int,pair<int,int>> p = q.front();
+            pair<pair<int,int>,int> p = q.front();
             q.pop();
-            int currTime = p.first;
-            ans = currTime;
-            int currX = p.second.first;
-            int currY = p.second.second;
+            int currX = p.first.first;
+            int currY = p.first.second;
+            int currTime = p.second;
+            cnt  = currTime;
             for(auto it:directions){
                 int newX = currX + it.first;
                 int newY = currY + it.second;
-                if(newX>=0 && newX<m && newY>=0 && newY<n && grid[newX][newY]==1 && !visited[newX][newY]){
-                    visited[newX][newY]  = 1;
-                    q.push({currTime+1,{newX,newY}});
+                if(newX>=0 && newX<grid.size() && newY>=0 && newY<grid[0].size() && grid[newX][newY]==1 && !visited[newX][newY]){
+                    visited[newX][newY] = 1;
+                    q.push({{newX,newY},currTime+1});
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<grid.size();i++){
+
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==1 && visited[i][j]==0)
                 return -1;
             }
         }
-        return ans;
+        return cnt;
     }
     int orangesRotting(vector<vector<int>>& grid) {
+        vector<pair<int,int>>directions = {{-1,0},{1,0},{0,1},{0,-1}};
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>>visited(m,vector<int>(n,0));
-        vector<pair<int,int>> directions = {{0,1},{0,-1},{-1,0},{1,0}};
-        return bfs(visited,grid,directions);
+        return bfs(visited,directions,grid);
     }
 };
