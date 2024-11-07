883cab65b4ecb38e18c4eb15b242bff308327177
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-11-08 00:12:14 +0530

    Time: 102 ms (13.47%) | Memory: 33.7 MB (28.16%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int>dp(n);
-        dp[0] = 1;
-        int p2 = 0;
-        int p3 = 0;
-        int p5 = 0;
-        for(int i=1;i<n;i++){
-            dp[i] = min({dp[p2]*2,dp[p3]*3,dp[p5]*5});
-            if(dp[i]==dp[p2]*2)
-            p2++;
-            if(dp[i]==dp[p3]*3)
-            p3++;
-            if(dp[i]==dp[p5]*5)
-            p5++;
+        map<long long int,int>m;
+        m[1]++;
+        auto it = m.begin();
+        int cnt = 0;
+        while(true){
+            auto it = m.begin();
+            long long int ele = it->first;
+            m.erase(ele);
+            cnt++;
+            if(cnt == n)
+            return ele;
+            m[ele*1LL*2]++;
+            m[ele*1LL*3]++;
+            m[ele*1LL*5]++;
         }
-        return dp[n-1];
+        return -1;
     }
 };
