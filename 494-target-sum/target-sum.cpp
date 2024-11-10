dc4ecc18c9e148fe452aa30967d78942d3b8dc9a
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-11-10 22:21:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (40.28%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int f(int idx, int target, vector<int>& nums,vector<vector<int>>&dp) {
-    if (idx == 0) {
-        if(target==0 && nums[0]==0) return 2;
-        return abs(target) == abs(nums[0]);
-    }
-    if(dp[idx][target+2000]!=-1) return dp[idx][target+2000];
-    int plus = f(idx - 1, target - nums[idx], nums,dp);
-    int minus = f(idx - 1, target + nums[idx], nums,dp);
-    
-    return dp[idx][target+2000]= plus + minus;
-}
+    int f(int ind, int target, vector<int>&nums,vector<vector<int>>&dp){
+        if(ind == 0){
+            if(nums[0]==0 && target == 0)
+            return 2;
+            else if(target == 0 || nums[0]==target)
+            return 1;
+            return 0;
+        }
+        if(dp[ind][target]!=-1)
+        return dp[ind][target];
 
-int findTargetSumWays(vector<int>& nums, int tar) {
-    int n = nums.size();
-    vector<vector<int>>dp(n,vector<int>(4001,-1));
-    return f(n - 1, tar, nums,dp);
-}
+        int notTake = f(ind-1,target,nums,dp);
+        int take = 0;
+        if(nums[ind]<=target)
+        take = f(ind-1,target-nums[ind],nums,dp);
+        return dp[ind][target] = take + notTake;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int totSum = 0;
+        for(auto it:nums){
+            totSum+=it;
+        }
+        target = abs(target);
+        if((totSum+target)%2 == 1)
+        return 0;
+        int tar = (totSum + target)/2;
+        vector<vector<int>>dp(n,vector<int>(tar+1,-1));
+        return f(n-1,tar,nums,dp);
+    }
 };
