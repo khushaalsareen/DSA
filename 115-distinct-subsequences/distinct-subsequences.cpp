a0c28eadc66bafd7b01cdda0d8d02658c5434fbf
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-11-12 00:27:15 +0530

    Time: 38 ms (29.03%) | Memory: 25.7 MB (64.54%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,24 +1,23 @@
 class Solution {
-    int f(int ind1, int ind2, string &s1, string &s2,vector<vector<int>>&dp){
-    if(ind1<0 &&  ind2<0)
-    return 1;
-    if(ind2<0)
-    return 1;
-    if(ind1<0)
-    return 0;
-    if(dp[ind1][ind2]!=-1)
-    return dp[ind1][ind2];
-    
-    if(s1[ind1]==s2[ind2])
-    return dp[ind1][ind2] = f(ind1-1,ind2-1,s1,s2,dp) + f(ind1-1,ind2,s1,s2,dp);
-    else
-    return dp[ind1][ind2] = f(ind1-1,ind2,s1,s2,dp); 
-}
 public:
-    int numDistinct(string A, string B) {
-          int n = A.size();
-    int m = B.size();
-    vector<vector<int>>dp(n,vector<int>(m,-1));
-    return f(n-1,m-1,A,B,dp);
+    int f(int ind1, int ind2, string &s, string &t,vector<vector<int>>&dp){
+        if(ind2<0)
+        return 1;
+        if(ind2>=0 && ind1<0)
+        return 0;
+        if(dp[ind1][ind2]!=-1)
+        return dp[ind1][ind2];
+
+        if(s[ind1]==t[ind2]){
+            return dp[ind1][ind2] = f(ind1-1,ind2-1,s,t,dp) + f(ind1-1,ind2,s,t,dp);
+        }
+        else
+            return dp[ind1][ind2] = f(ind1-1,ind2,s,t,dp);
+    }
+    int numDistinct(string s, string t) {
+        int s1 = s.size();
+        int s2 = t.size();
+        vector<vector<int>>dp(s1,vector<int>(s2,-1));
+        return f(s.size()-1,t.size()-1,s,t,dp);
     }
 };
