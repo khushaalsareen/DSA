7b30aa6a1882f5851e1d413756684452c0fec836
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-11-12 00:51:07 +0530

    Time: 83 ms (46.30%) | Memory: 11.9 MB (42.13%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int f(int ind, int target, vector<vector<int>>&dp){
-        if(target == 0)
+    int f(int ind,vector<int>&dp){
+        if(ind == 0)
         return 0;
-        if(ind == 1)
-        return target;
-        if(dp[ind][target]!=-1)
-        return dp[ind][target];
-        int nottake = f(ind-1,target,dp);
-        int take = 1e9;
-        if(ind*ind<=target)
-         take = 1 + f(ind, target - ind*ind,dp);
-        return dp[ind][target] = min(take,nottake);
+        if(dp[ind]!=-1)
+        return dp[ind];
+
+        int mini = 1e9;
+        for(int i=1;i*i<=ind;i++){
+            int coins = 1 + f(ind - i*i,dp);
+            mini =  min(mini,coins);
+        }
+        return dp[ind] = mini;
     }
     int numSquares(int n) {
-            int num = sqrt(n);
-            if(num == 1)
-            return n;
-            vector<vector<int>>dp(num+1,vector<int>(n+1,-1));
-            return f(num,n,dp);
+        vector<int>dp(n+1,-1);
+        return f(n,dp);
     }
 };
