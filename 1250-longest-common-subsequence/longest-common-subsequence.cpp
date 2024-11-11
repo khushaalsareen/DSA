3c74eeedf40ff7357c07bbabc12eb8b4d299bfae
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-11-11 12:06:27 +0530

    Time: 42 ms (26.35%) | Memory: 26.3 MB (8.07%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int f(int ind1, int ind2, string &s1, string &s2,vector<vector<int>>&dp){
-        if(ind1 <0 || ind2<0)
+        if(ind1 ==0 || ind2==0)
         return 0;
         if(dp[ind1][ind2]!=-1)
         return dp[ind1][ind2];
 
-        if(s1[ind1]==s2[ind2])
+        if(s1[ind1-1]==s2[ind2-1])
         return 1 + f(ind1-1,ind2-1,s1,s2,dp);
         else
         return dp[ind1][ind2]= max(f(ind1-1,ind2,s1,s2,dp),f(ind1,ind2-1,s1,s2,dp));
@@ -14,7 +14,8 @@ public:
     int longestCommonSubsequence(string s1, string s2) {
         int n1 = s1.size();
         int n2 = s2.size();
-        vector<vector<int>>dp(n1,vector<int>(n2,-1));
-        return f(n1-1,n2-1,s1,s2,dp);
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
+        return f(n1,n2,s1,s2,dp);
+
     }
 };
