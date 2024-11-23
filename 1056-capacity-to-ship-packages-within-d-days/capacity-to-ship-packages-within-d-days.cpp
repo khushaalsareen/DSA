c138ad8373ed8e97d26733e08dcd6f9e977e54fd
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-11-23 18:31:27 +0530

    Time: 7 ms (92.96%) | Memory: 33.8 MB (80.68%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -2,40 +2,35 @@ class Solution {
 public:
     bool f(int mid, vector<int>&nums, int days){
         int cnt = 1;
-        int load = 0;
-        for(int i=0;i<nums.size();i++){
-        if(load + nums[i]<=mid){
-                load+=nums[i];
+        int wt = nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(wt + nums[i]<=mid){
+                wt+=nums[i];
             }
             else{
                 cnt++;
-                load = nums[i];
+                wt = nums[i];
             }
         }
-
-        if(cnt>days)
-        return false;
-        return true;
+        return cnt<=days;
     }
-    int shipWithinDays(vector<int>&nums, int days) {
-        int n = nums.size();
+
+    int shipWithinDays(vector<int>& nums, int days) {
         int l = *max_element(nums.begin(),nums.end());
-        int h;
-        int sum = 0;
-        for(auto it:nums){
-            sum+=it;
-        }
-        h = sum;
+        int h = 0;
+        for(auto it:nums)
+        h+=it;
         int mid;
         int ans;
         while(l<=h){
             mid = l+(h-l)/2;
-            if(f(mid,nums,days)){
-                ans = mid;
+            bool check = f(mid,nums,days);
+            if(check){
+                ans =mid;
                 h = mid-1;
             }
             else
-            l = mid+1;
+                l = mid + 1;
         }
         return ans;
     }
