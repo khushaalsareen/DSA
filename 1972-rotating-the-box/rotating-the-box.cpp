ab37c6f15a44d2a111f16ebd29e9758c8e270d30
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-11-23 15:17:03 +0530

    Time: 189 ms (69.58%) | Memory: 56.2 MB (85.37%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -1,66 +1,39 @@
 class Solution {
 public:
-    vector<vector<char>> rotate(vector<vector<char>>& box){
-        int m = box.size();
-        int n = box[0].size();
-        //m*n matrix
-        //n*m matrix
-        vector<vector<char>>ans(n,vector<char>(m));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans[j][i] = box[i][j];
-            }
+    vector<vector<char>> rotate(vector<vector<char>>& box) {
+    int m = box.size();
+    int n = box[0].size();
+    // Transpose and reverse columns for 90° clockwise rotation
+    vector<vector<char>> ans(n, vector<char>(m));
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            ans[j][m - i - 1] = box[i][j];
         }
+    }
+    return ans;
+}
 
-        int i = 0;
-        int j = ans[0].size()-1;
-        while(i<j){
-            for(int r=0;r<ans.size();r++){
-                swap(ans[r][i],ans[r][j]);
+vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+    // Step 1: Rotate the box 90° clockwise
+    vector<vector<char>> ans = rotate(box);
+    int m = ans.size();    // New number of rows
+    int n = ans[0].size(); // New number of columns
+
+    // Step 2: Simulate gravity in each column
+    for (int j = 0; j < n; j++) { // For each column
+        int emptyRow = m - 1; // The lowest available empty position
+        for (int i = m - 1; i >= 0; i--) { // Traverse upwards in the column
+            if (ans[i][j] == '*') { 
+                // Obstacle found; reset emptyRow above it
+                emptyRow = i - 1;
+            } else if (ans[i][j] == '#') {
+                // Stone found; move it to the lowest available position
+                swap(ans[i][j], ans[emptyRow][j]);
+                emptyRow--; // Update the lowest available position
             }
-            i++;
-            j--;
         }
-        return ans;
     }
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        vector<vector<char>>ans = rotate(box);
-        int m = ans.size();
-        int n = ans[0].size();
-        int j = n-1;
-        while(j>=0){
-            //for each column
-            vector<int>NGE(m,m);
-            stack<int>s;
-            s.push(0);
-            for(int i=1;i<m;i++){
-                // index i is next obstacle of how many
-                while(!s.empty() && ans[i][j]=='*'){
-                    NGE[s.top()] = i;
-                    s.pop();
-                }
-                s.push(i);
-            }
-            unordered_map<int,int>mp;
-            int lastSafe;
-            for(int i=m-1;i>=0;i--){
-                if(ans[i][j]=='#') // a stone
-                {
-                    int nextObs = NGE[i];
-                    if(mp.find(nextObs)!=mp.end()){
-                        lastSafe--;
-                        swap(ans[i][j],ans[lastSafe][j]);
-                    }
-                    else{
-                        lastSafe = NGE[i]-1;
-                        mp[nextObs]++;
-                        swap(ans[i][j],ans[lastSafe][j]);
-                        
-                    }
-                }
-            }
-                j--;
-        }
-        return ans;
-    }
+    return ans;
+}
+
 };
