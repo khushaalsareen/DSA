4b1e060b2fb63e487b742a0d4fbba53c9c5260b3
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-11-23 17:56:34 +0530

    Time: 11 ms (47.04%) | Memory: 21.8 MB (43.79%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    bool isPossible(int mid, vector<int>& piles, int h){
-        long long int tot_time = 0;
-        for(auto it:piles){
-            tot_time += ceil(it/(mid*1.0))*1LL;
+    bool isPossible(int mid, vector<int>&piles, int h){
+       long long int time = 0;
+        for(auto &it:piles){
+            time+=ceil(it/(mid*1LL*1.0));
         }
-        return tot_time<=h;
+        return time<=h;
     }
-    int minEatingSpeed(vector<int>& piles, int hrs) {
+    int minEatingSpeed(vector<int>& piles, int hr) {
         int l = 1;
         int h = *max_element(piles.begin(),piles.end());
         int mid;
         int ans;
         while(l<=h){
             mid = l+(h-l)/2;
-            if(isPossible(mid,piles,hrs)){
+            bool check = isPossible(mid,piles,hr);
+            if(check){
                 ans = mid;
-                h = mid-1;
+                h = mid - 1;
             }
             else
-            l = mid+1;
+                l = mid + 1;
         }
         return ans;
     }
