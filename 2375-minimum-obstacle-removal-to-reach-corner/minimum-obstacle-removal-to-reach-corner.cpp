a7bf283865c48767d08814d55cad29737e2f6464
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-11-28 10:26:32 +0530

    Time: 114 ms (92.90%) | Memory: 129.6 MB (54.40%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -3,29 +3,29 @@ public:
     int bfs(int sx, int sy, vector<pair<int,int>>&directions, vector<vector<int>>&grid,vector<vector<int>>& visited){
         int m = grid.size();
         int n = grid[0].size();
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>q;
-        q.push({0,{0,0}});
+        deque<pair<int,pair<int,int>>>q;
+        q.push_front({0,{0,0}});
         visited[sx][sy] = 1;
         int ans = 1e9;
         while(!q.empty()){
-            pair<int,pair<int,int>> p  = q.top();
-            q.pop();
+            pair<int,pair<int,int>> p  = q.front();
+            q.pop_front();
             int currX = p.second.first;
             int currY = p.second.second;
             int currObstacles = p.first;
             if(currX == m-1 && currY == n-1){
-                return currObstacles;
+                ans = min(ans,currObstacles);
             }
             for(int i=0;i<4;i++){
                 int newX = currX + directions[i].first;
                 int newY = currY + directions[i].second;
                 if(newX>=0 && newX<m && newY>=0 && newY<n && !visited[newX][newY]){
                     if(grid[newX][newY]==1){
-                        q.push({currObstacles+1,{newX,newY}});
+                        q.push_back({currObstacles+1,{newX,newY}});
                         visited[newX][newY] = 1;
                     }
                     else{
-                        q.push({currObstacles,{newX,newY}});
+                        q.push_front({currObstacles,{newX,newY}});
                         visited[newX][newY] = 1;
                     }
                 }
