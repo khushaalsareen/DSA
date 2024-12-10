b1f3acd9f8df158226395a4a80c12b9a4597dd43
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2024-12-10 16:35:52 +0530

    Time: 2857 ms (5.26%) | Memory: 122.2 MB (5.05%) - LeetSync

---
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<long long int>psum(n);
+        psum[0] = nums[0]*1LL;
+        for(int i=1;i<n;i++){
+            psum[i] = psum[i-1] + nums[i];
+        }
+        int minLen = 1e9;
+        deque<pair<long long int,int>>dq;
+        dq.push_back({0,-1});
+        for(int i=0;i<n;i++){
+            // int currVal = psum[i];
+            // cout<<currVal<<endl;
+            cout<<dq.front().first<<endl;
+            cout <<!dq.empty() && ((psum[i]-dq.front().first)>=k);
+            while(!dq.empty() && ((psum[i]-dq.front().first)>=k)){
+        
+                minLen = min(minLen,i-dq.front().second);
+                dq.pop_front();
+            }
+            while(!dq.empty() && psum[i]<=dq.back().first)
+            dq.pop_back();
+            dq.push_back({psum[i],i});
+        }
+        return minLen == 1e9? -1 : minLen;
+    }
+};
