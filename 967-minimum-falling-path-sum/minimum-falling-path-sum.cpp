52fe6d94bb3374e793c5b3a2ff5fe4299c1dfb4d
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-01-13 00:28:08 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (14.30%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
-    int f(int i, int j,vector<vector<int>>& matrix,vector<vector<int>>&dp ){
-        int n = matrix.size();
-        if(j>=n ||j<0) return 1e9;
-        if(i==n-1) return matrix[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = matrix[i][j] + f(i+1,j,matrix,dp);
-        int right = matrix[i][j] + f(i+1,j+1,matrix,dp);
-        int left = matrix[i][j] + f(i+1,j-1,matrix,dp);
-        return dp[i][j] = min({down,left,right});
+    int f(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        int m = grid.size();
+        int n = grid[0].size();
+        if (j < 0 || j >= n)
+            return 1e9;
+        if (i == m - 1)
+            return grid[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int left = grid[i][j] + f(i + 1, j - 1, grid, dp);
+        int down = grid[i][j] + f(i + 1, j, grid, dp);
+        int right = grid[i][j] + f(i + 1, j + 1, grid, dp);
+
+        return dp[i][j] = min({left, down, right});
     }
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
         int ans = 1e9;
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        // for(int j=0;j<matrix.size();j++){
-        //     ans = min(ans,f(0,j,matrix,dp));
-        // }
-        for(int j=0;j<n;j++){
-            dp[n-1][j] = matrix[n-1][j];
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int j = 0; j < n; j++) {
+            dp[m - 1][j] = grid[m - 1][j];
         }
-        for(int i=n-2;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                int down = matrix[i][j] + dp[i+1][j];// f(i+1,j,matrix,dp);
-                int right = matrix[i][j]; 
-                if(j+1<n)
-                right+= dp[i+1][j+1];// f(i+1,j+1,matrix,dp);
-                else
-                right+=1e9;
-                int left = matrix[i][j]; 
-                if(j-1>=0)
-                left+= dp[i+1][j-1];// f(i+1,j-1,matrix,dp);
-                else
-                left+=1e9;
-                dp[i][j] = min({down,left,right});
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int left = grid[i][j] + (j-1>=0?dp[i+1][j-1]:1e8);// f(i + 1, j - 1, grid, dp);
+                int down = grid[i][j] + dp[i+1][j];
+                int right = grid[i][j] + (j+1<n?dp[i+1][j+1]:1e8);// f(i + 1, j + 1, grid, dp);
+
+                 dp[i][j] = min({left, down, right});
             }
         }
-        return *min_element(dp[0].begin(),dp[0].end());
+
+        for (int i = 0; i < n; i++) {
+            ans = min(ans, dp[0][i]);
+        }
+        return ans;
     }
 };
