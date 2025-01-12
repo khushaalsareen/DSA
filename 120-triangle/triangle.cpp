56a59b10ea430a550126d6b7b602bfe9a76982d0
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-01-12 23:35:10 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (48.72%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& triangle,vector<vector<int>>&dp){
-        int n = triangle.size();
-        if(i==n-1)
-        return triangle[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = triangle[i][j] + f(i+1,j,triangle,dp);
-        int right = triangle[i][j] + f(i+1,j+1,triangle,dp);
+    int f(int i, int j, vector<vector<int>>& grid,vector<vector<int>>&dp){
+        int m = grid.size();
+        if(i == m-1)
+        return grid[i][j];
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int down = grid[i][j] + f(i+1,j,grid,dp);
+        int right = grid[i][j] + f(i+1,j+1,grid,dp);
         return dp[i][j] = min(down,right);
     }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>>dp(triangle.size(),vector<int>(triangle.size(),0));
-        // return f(0,0,triangle,dp);
-        for(int j=0;j<triangle.size();j++){
-            dp[triangle.size()-1][j] = triangle[triangle.size()-1][j];
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=triangle[i].size()-1;j>=0;j--){
-                int down = triangle[i][j] + dp[i+1][j];
-                 int right = triangle[i][j] + dp[i+1][j+1];// f(i+1,j+1,triangle,dp);
-                dp[i][j] = min(down,right);
-            }
-        }
-        return dp[0][0];
+    int minimumTotal(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(m,-1));
+        return f(0,0,grid,dp);
     }
 };
