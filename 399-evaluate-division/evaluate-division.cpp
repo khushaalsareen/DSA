6654ee399a07597fabdaf45180412dd42c6cb144
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-02-08 23:09:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (42.56%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    double bfs(string start, string end,unordered_map<string,vector<pair<string,double>>>&m ){
+        queue<pair<string,double>>q;
+        set<string>s;
+        if (m.find(start) == m.end() || m.find(end) == m.end()) return -1.0;
+        q.push({start,1});
+        s.insert(start);
+        while(!q.empty()){
+            pair<string,double> p = q.front();
+            q.pop();
+            string currString = p.first;
+            double currVal = p.second;
+            if(currString == end)
+            return currVal;
+            for(auto it:m[currString]){
+                if(s.find(it.first)==s.end()){
+                    s.insert(it.first);
+                    q.push({it.first,currVal * it.second});
+                }
+            }
+        }
+        return -1;
+    }
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        vector<double>ans;
+        int n = equations.size();
+        unordered_map<string,vector<pair<string,double>>>m;
+        for(int i=0;i<n;i++){
+            vector<string>&it = equations[i];
+            string &u = it[0];
+            string &v = it[1];
+            m[u].push_back({v,values[i]});
+            double reci = 1/values[i];
+            m[v].push_back({u,reci});
+        }
+        int q = queries.size();
+        for(auto it:queries){
+            double res = bfs(it[0],it[1],m);
+            ans.push_back(res);
+        }
+        return ans;
+    }
+};
