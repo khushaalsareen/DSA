4b480cfccd25a8728b08374726ad9026ef49add1
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-03-07 01:58:26 +0530

    Time: 13 ms (14.57%) | Memory: 11.9 MB (25.31%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool is_valid(int r, int c, vector<string>&v, vector<int>&cols){
+    bool is_valid(int r, int c, vector<string>&v, unordered_set<int>&cols,unordered_set<int>&ld,unordered_set<int>&rd){
         
         for(int i=0;i<r;i++){
-            if(cols[i]==c || abs(r-i)==abs(c-cols[i]))
+            if(cols.find(c)!=cols.end() || ld.find(r-c)!=ld.end() || rd.find(r+c)!=rd.end() )
             return false;
         }
         return true;
     }
-    void generate(int r, vector<string>&v, vector<vector<string>>&ans,vector<int>&cols){
+    void generate(int r, vector<string>&v, vector<vector<string>>&ans,unordered_set<int>&cols,unordered_set<int>&ld,unordered_set<int>&rd){
         if(r == v.size()){
             ans.push_back(v);
             return;
@@ -16,20 +16,26 @@ public:
         // do all stuff on current level
         // i can try to plcae queen from 0 to n-1
         for(int c = 0;c<v.size();c++){
-            if(is_valid(r,c,v,cols)){
+            if(is_valid(r,c,v,cols,ld,rd)){
                 v[r][c] = 'Q';
-                cols[r] = c;
-                generate(r+1,v,ans,cols);
+                cols.insert(c);
+                ld.insert(r-c);
+                rd.insert(r+c);
+                generate(r+1,v,ans,cols,ld,rd);
                 v[r][c] = '.'; // backyracking
-                cols[r] = -1;
+                cols.erase(c);
+                ld.erase(r-c);
+                rd.erase(r+c);
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string>v(n,string(n,'.'));
-        vector<int>cols(n,-1);
-        generate(0,v,ans,cols);
+        unordered_set<int>cols;
+        unordered_set<int>ld;
+        unordered_set<int>rd;
+        generate(0,v,ans,cols,ld,rd);
         return ans;
     }
 };
