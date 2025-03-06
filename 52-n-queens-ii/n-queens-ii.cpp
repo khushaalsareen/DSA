c889eb19cb54a88c5f81793aab2928e4654035ba
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-03-07 02:10:19 +0530

    Time: 11 ms (11.23%) | Memory: 9.7 MB (14.09%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 
-    bool is_valid(int r, int c, vector<string>&v, unordered_set<int>&cols,unordered_set<int>&ld,unordered_set<int>&rd){
+    bool is_valid(int r, int c, unordered_set<int>&cols,unordered_set<int>&ld,unordered_set<int>&rd){
         
             if(cols.find(c)!=cols.end() || ld.find(r-c)!=ld.end() || rd.find(r+c)!=rd.end() )
             return false;
         
         return true;
     }
-    void generate(int r, vector<string>&v,unordered_set<int>&cols,unordered_set<int>&ld,unordered_set<int>&rd, int &cnt){
-        if(r == v.size()){
+    void generate(int r,unordered_set<int>&cols,unordered_set<int>&ld,unordered_set<int>&rd, int &cnt, int n){
+        if(r == n){
             // ans.push_back(v);
             cnt++;
             return;
         }
         // do all stuff on current level
         // i can try to plcae queen from 0 to n-1
-        for(int c = 0;c<v.size();c++){
-            if(is_valid(r,c,v,cols,ld,rd)){
-                v[r][c] = 'Q';
+        for(int c = 0;c<n;c++){
+            if(is_valid(r,c,cols,ld,rd)){
+                // v[r][c] = 'Q';
                 cols.insert(c);
                 ld.insert(r-c);
                 rd.insert(r+c);
-                generate(r+1,v,cols,ld,rd,cnt);
-                v[r][c] = '.'; // backyracking
+                generate(r+1,cols,ld,rd,cnt,n);
+                // v[r][c] = '.'; // backyracking
                 cols.erase(c);
                 ld.erase(r-c);
                 rd.erase(r+c);
@@ -31,12 +31,12 @@ class Solution {
     }
     int solveNQueens(int n) {
         // vector<vector<string>> ans;
-        vector<string>v(n,string(n,'.'));
+        // vector<string>v(n,string(n,'.'));
         unordered_set<int>cols;
         unordered_set<int>ld;
         unordered_set<int>rd;
         int cnt = 0;
-        generate(0,v,cols,ld,rd,cnt);
+        generate(0,cols,ld,rd,cnt,n);
         return cnt;
     }
 
