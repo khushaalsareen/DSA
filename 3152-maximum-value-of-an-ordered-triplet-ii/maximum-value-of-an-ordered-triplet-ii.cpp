8f3a30f302c079d7e3b06cc3fa51b957809200ac
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-04-04 00:39:37 +0530

    Time: 19 ms (8.36%) | Memory: 105.5 MB (5.01%) - LeetSync

---
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+      int n = nums.size();
+      vector<int>pmax(n);
+      vector<int>smax(n);
+      pmax[0] = nums[0];
+      for(int i=1;i<n;i++){
+        pmax[i] = max(pmax[i-1],nums[i]);
+      }   
+      smax[n-1] = nums[n-1];
+      for(int i=n-2;i>=0;i--){
+        smax[i] = max(smax[i+1],nums[i]);
+      }
+      vector<int>pmin(n);
+      vector<int>smin(n);
+      pmin[0] = nums[0];
+      for(int i=1;i<n;i++)
+        pmin[i] = min(pmin[i-1],nums[i]);
+      smin[n-1] = nums[n-1];
+      for(int i=n-2;i>=0;i--)
+        smin[i] = min(smin[i+1],nums[i]);
+
+      long long ans = 0;
+      for(int m=1;m<n-1;m++){
+        long long v1 = (pmax[m-1]-nums[m])*1LL*smax[m+1];
+        long long v2 = (pmax[m-1]-nums[m])*1LL*smin[m+1];
+        long long v3 = (pmin[m-1]-nums[m])*1LL*smin[m+1];
+        long long v4 = (pmin[m-1]-nums[m])*1LL*smax[m+1];
+        ans = max({ans,v1,v2,v3,v4});
+      }
+      return ans;
+    }
+};
