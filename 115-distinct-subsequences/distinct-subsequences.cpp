db042aeea9f8e288a4027cbd539375875994ea01
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-04-18 15:22:07 +0530

    Time: 38 ms (22.59%) | Memory: 26.9 MB (64.24%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -3,21 +3,23 @@ public:
     int f(int ind1, int ind2, string &s, string &t,vector<vector<int>>&dp){
         if(ind2<0)
         return 1;
-        if(ind2>=0 && ind1<0)
+        if(ind1<0)
         return 0;
         if(dp[ind1][ind2]!=-1)
         return dp[ind1][ind2];
 
         if(s[ind1]==t[ind2]){
-            return dp[ind1][ind2] = f(ind1-1,ind2-1,s,t,dp) + f(ind1-1,ind2,s,t,dp);
+            // either match with this or wait
+            return dp[ind1][ind2] = f(ind1-1,ind2-1,s,t,dp)  + f(ind1-1,ind2,s,t,dp);
         }
-        else
+        else{
             return dp[ind1][ind2] = f(ind1-1,ind2,s,t,dp);
+        }
     }
     int numDistinct(string s, string t) {
-        int s1 = s.size();
-        int s2 = t.size();
-        vector<vector<int>>dp(s1,vector<int>(s2,-1));
-        return f(s.size()-1,t.size()-1,s,t,dp);
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,s,t,dp);
     }
 };
