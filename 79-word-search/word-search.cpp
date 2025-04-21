54676d880faaabb42bc3882363dfe8d250492125
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-04-21 23:18:07 +0530

    Time: 865 ms (18.52%) | Memory: 11.5 MB (47.68%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,36 +1,37 @@
 class Solution {
-    vector<pair<int,int>>directions = {{1,0},{-1,0},{0,1},{0,-1}};
 public:
-    bool dfs(int i, int j, vector<vector<char>>&board, string &word, int ind){
-        if(ind+1 == word.size())
-        return true;
-
-        char ch = board[i][j];
-        board[i][j] = '#';
-        for(auto it:directions){
-            int newX = i + it.first;
-            int newY = j + it.second;
-            if(newX>=0 && newY>=0 && newX<board.size() && newY<board[0].size() && word[ind+1]==board[newX][newY]){
-                if(dfs(newX,newY,board,word,ind+1))
-                return true;
-            }
-        }
-        board[i][j] = ch;
-        return false;
+    //pass board by reference
+    bool DFS(vector<vector<char>>& board, string word, int i, int j, int n) {
+		//check if all the alphabets in the word is checked
+        if(n == word.size()) return true; 
+        
+		//check if i and j are out of bound or if the characters aren't equal
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[i].size() || board[i][j] != word[n]) return false;
+        
+		//mark as visited 
+        board[i][j] = '0';
+        
+		//branch out in all 4 directions
+        bool status = DFS(board, word, i + 1, j, n + 1) ||  //down
+                        DFS(board, word, i, j + 1, n + 1) ||  //right
+                        DFS(board, word, i - 1, j, n + 1) ||  //up
+                        DFS(board, word, i, j - 1, n + 1);  //left
+        
+		//change the character back for other searches
+        board[i][j] = word[n];
+		
+        return status;
     }
+    
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        vector<pair<int,int>>directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]==word[0]){
-                    if(dfs(i,j,board,word,0)){
-                        return true;
-                    }
-                }
-            }
-        }
+        if(word == "") return false;
+        
+        for(int i = 0; i < board.size(); i++) 
+            for(int j = 0; j < board[i].size(); j++) 
+				//check if the characters are equal then call DFS
+                if(board[i][j] == word[0] && DFS(board, word, i, j, 0))
+                    return true;
+        
         return false;
     }
 };
