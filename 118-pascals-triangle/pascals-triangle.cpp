19b57cda7953b3397a9e4973c845071241331691
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-04-26 22:49:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (64.75%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
-        vector<vector<int>> dp;
-        dp.push_back({1});
+        vector<vector<int>>ans;
+        ans.push_back({1});
         for(int i=1;i<n;i++){
-            vector<int>v(i+1);
-            v[0] = 1;
-            for(int j=1;j<=i-1;j++){
-                v[j] = dp[i-1][j] + dp[i-1][j-1];
+            vector<int>v(i+1,1);
+            ans.push_back(v);
+            for(int j=1;j<i;j++){
+                ans[i][j] = ans[i-1][j] + ans[i-1][j-1];
             }
-            v[i] = 1;
-            dp.push_back(v);
         }
-        return dp;
+        return ans;
     }
 };
