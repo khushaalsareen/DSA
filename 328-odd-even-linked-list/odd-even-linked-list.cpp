95abcb90c455fd2706c33b54d2c3d29c3df53959
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-05-03 02:30:18 +0530

    Time: 2 ms (4.00%) | Memory: 15.6 MB (28.60%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,37 +1,39 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(!head)
-        return NULL;
-        if(head->next == NULL)
-        return head;
-        ListNode * head1 = head, *head2 = head->next;
-        ListNode*tail1 = head1, *tail2 = head2;
-        ListNode*tmp1 = head1, *tmp2 = head2;
-        while(tmp1 &&tmp2){
-            tmp1->next = tmp2->next;
-            tail1 = tmp1;
-            tmp1 = tmp1->next;
-            if(tmp1){
-                tmp2->next = tmp1->next;
-                tail2 = tmp2;
-                tmp2 = tmp2->next;
+        int cnt = 1;
+        ListNode*temp1=NULL,*temp2=NULL,*head1=NULL,*head2=NULL;
+        if(!head) return NULL;
+        if(!head->next) return head;
+        ListNode*temp = head;
+        while(temp){
+            if(cnt%2 == 0) // even
+            {
+                if(temp2==NULL){
+                temp2 = temp;
+                head2 = temp;
+                }
+                else{
+                temp2->next = temp;
+                temp2 = temp2->next;
+                }
             }
+            else {
+                if(temp1==NULL){
+                temp1 = temp;
+                head1 = temp;
+                }
+                else{
+                temp1->next = temp;
+                temp1 = temp1->next;
+                }
+            }
+            temp = temp->next;
+            cnt+=1;
         }
-        if(tmp1)
-        tmp1->next = head2;
-        else
-        tail1->next = head2;
+        temp1->next = NULL;
+        temp2->next = NULL;
+        temp1->next = head2;
         return head1;
     }
 };
