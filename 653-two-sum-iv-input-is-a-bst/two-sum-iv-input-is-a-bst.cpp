8adc8693bb3c51a19ec3c475eee42038c9bd2934
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-05-11 17:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 38.1 MB (35.06%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -9,58 +9,75 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class BSTIterator {
+    stack<TreeNode*>s;
+    stack<TreeNode*>s2;
+    void leftPush(TreeNode*root){ // this is basically doing in ascending order pushing on stack
+        if(!root)
+        return;
+        TreeNode * tmp = root;
+        while(tmp){
+            s.push(tmp);
+            tmp = tmp->left;
+        }
+    }
+    void rightPush(TreeNode*root){
+        if(!root)
+        return;
+        TreeNode*tmp = root;
+        while(tmp){
+            s2.push(tmp);
+            tmp = tmp->right;
+        }        
+    }
+public:
+    BSTIterator(TreeNode* root) {
+        leftPush(root);
+        rightPush(root);
+    }
+    
+    int next() {
+        TreeNode * nxtNode = s.top();
+        s.pop();
+        leftPush(nxtNode->right);
+        return nxtNode->val;
+    }
+    
+    bool hasNext() {
+        return s.size()>0;
+    }
+
+    int prev(){
+        TreeNode * prevNode = s2.top();
+        s2.pop();
+        rightPush(prevNode->left);
+        return prevNode->val;
+    }
+
+    int hasPrev(){
+        return s2.size()>0;
+    }
+};
+
 class Solution {
 public:
-    TreeNode*findPred(TreeNode*root, int value){
-        TreeNode*ans = NULL;
-        TreeNode*tmp = root;
-        while(tmp){
-            if(tmp->val < value){
-                ans = tmp;
-                tmp = tmp->right;
-            }
-            else
-                tmp = tmp->left;
-        }
-        return ans;
-    }
-
-    TreeNode* findSucc(TreeNode*root, int value){
-        TreeNode*ans = NULL;
-        TreeNode*tmp = root;
-        while(tmp){
-            if(tmp->val > value){
-                ans = tmp;
-                tmp = tmp->left;
-            }
-            else
-                tmp = tmp->right;
-        }
-        return ans;
-    }
-
     bool findTarget(TreeNode* root, int k) {
         if(!root->left && !root->right)
         return false;
 
-        TreeNode*leftNode = root;
-        TreeNode*rightNode = root;
-        while(leftNode->left){
-            leftNode = leftNode->left;
-        }
-        while(rightNode->right){
-            rightNode = rightNode->right;
-        }
-        // both left and right point to extreme points
-        while(leftNode->val < rightNode->val){
-            int sum = leftNode->val + rightNode->val;
-            if(sum == k)
+        BSTIterator l(root);
+        BSTIterator r(root);
+
+        int i = l.next();
+        int j = r.prev();
+        while(i<j){
+            if(i+j == k)
             return true;
-            else if(sum>k){
-                rightNode = findPred(root,rightNode->val);
-            }
+            else if(i+j<k)
+            i = l.next();
             else
-                leftNode = findSucc(root,leftNode->val);
+            j = r.prev();
         }
         return false;
     }
