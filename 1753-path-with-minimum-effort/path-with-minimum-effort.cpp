972b972082fa2283b47276bc64d18b2a56d59a04
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-07-06 15:56:10 +0530

    Time: 65 ms (39.33%) | Memory: 24.5 MB (55.78%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,34 +1,33 @@
 class Solution {
+    vector<pair<int,int>>directions = {{1,0},{-1,0},{0,1},{0,-1}};
 public:
-    void dijkstra(vector<vector<int>>& heights, vector<vector<int>>&dist,vector<pair<int,int>>&directions){
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>pq;
-        pq.push({0,{0,0}});
+    int minimumEffortPath(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>>dist(m,vector<int>(n,1e9));
         dist[0][0] = 0;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        pq.push({0,{0,0}}); // {dist,{x,y}}
         while(!pq.empty()){
             pair<int,pair<int,int>>p = pq.top();
             pq.pop();
-            int currEffort = p.first;
+            int currDist = p.first;
             int currX = p.second.first;
             int currY = p.second.second;
+            
             for(auto it:directions){
                 int newX = currX + it.first;
                 int newY = currY + it.second;
-                if(newX<0 || newY<0 || newX>=heights.size() || newY>=heights[0].size())
-                continue;
-                int edgeWeight = abs(heights[newX][newY] - heights[currX][currY]);
-                int updatedEffort = max(edgeWeight, currEffort);
-                if(updatedEffort<dist[newX][newY]){
-                    dist[newX][newY] = updatedEffort;
-                    pq.push({updatedEffort,{newX,newY}});
+                if(newX>=0 && newY>=0 && newX<mat.size() && newY<mat[0].size()){
+                    // cell is valid
+                    int wt = abs(mat[currX][currY]-mat[newX][newY]);
+                    if(dist[newX][newY]>max(currDist,wt)){
+                        dist[newX][newY] = max(currDist,wt);
+                        pq.push({dist[newX][newY],{newX,newY}});
+                    }
                 }
             }
         }
-        // return dist;
-    }
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        vector<vector<int>>dist(heights.size(),vector<int>(heights[0].size(),1e9));
-        vector<pair<int,int>>directions = {{0,1},{0,-1},{-1,0},{1,0}};
-        dijkstra(heights,dist,directions);
-        return dist[heights.size()-1][heights[0].size()-1]; 
+        return dist[m-1][n-1];
     }
 };
