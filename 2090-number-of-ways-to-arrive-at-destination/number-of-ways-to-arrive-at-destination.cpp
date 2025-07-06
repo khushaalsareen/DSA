271311cbb31e8bd83ce8a1d75089f4b01bdc6105
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-07-06 16:37:19 +0530

    Time: 4 ms (83.88%) | Memory: 34.7 MB (99.31%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,48 +1,38 @@
 class Solution {
-    int m = 1e9 + 7;
+    const int mod = 1e9 + 7;
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
+        vector<long long> dist(n, LLONG_MAX);
         vector<pair<int, int>> adj[n];
+        vector<int> ways(n, 0);
 
-        // Build adjacency list
-        for (auto &it : roads) {
+        for (auto& it : roads) {
             int u = it[0], v = it[1], wt = it[2];
-            adj[u].push_back({v, wt});
-            adj[v].push_back({u, wt});
+            adj[u].emplace_back(v, wt);
+            adj[v].emplace_back(u, wt);
         }
 
-        // Distance array and ways array
-        vector<long long> dist(n, 1e18);
-        vector<int> ways(n, 0);
         dist[0] = 0;
         ways[0] = 1;
-
-        // Min-Heap for Dijkstra
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
         pq.push({0, 0});
 
         while (!pq.empty()) {
             auto [currDist, currNode] = pq.top();
             pq.pop();
 
-            
-
-            for (auto &[ngbr, ew] : adj[currNode]) {
-                long long newDist = currDist + ew;
-
-                // Found a shorter path
-                if (newDist < dist[ngbr]) {
-                    dist[ngbr] = newDist;
-                    ways[ngbr] = ways[currNode];  // Reset ways count for this node
-                    pq.push({newDist, ngbr});
-                }
-                // Found another shortest path
-                else if (newDist == dist[ngbr]) {
-                    ways[ngbr] = (ways[ngbr]%m + ways[currNode]%m) % m;
+            for (auto& [neighbor, wt] : adj[currNode]) {
+                long long newDist = currDist + wt;
+                if (newDist < dist[neighbor]) {
+                    dist[neighbor] = newDist;
+                    pq.push({newDist, neighbor});
+                    ways[neighbor] = ways[currNode];
+                } else if (newDist == dist[neighbor]) {
+                    ways[neighbor] = (ways[neighbor] + ways[currNode]) % mod;
                 }
             }
         }
 
-        return ways[n - 1]; // Number of ways to reach node n-1
+        return ways[n - 1];
     }
 };
