3ef56e514d4999d0d2def774780bc56e6fb9d8dc
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-07-06 22:26:22 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (67.10%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,31 +4,37 @@ public:
         int n = nums.size();
         int l = 0;
         int h = n-1;
-        int mid;
         if(n==1)
         return nums[0];
-
         while(l<=h){
-            mid = l+(h-l)/2;
-            if(mid%2 == 0){
-                if(mid == 0 && nums[mid]!=nums[mid+1])
-                return nums[0];
-                else if(mid == n-1 && nums[mid]!=nums[ mid-1])
+            int mid = l+(h-l)/2;
+            if(mid == 0){
+                if(nums[mid]==nums[mid+1])
+                l = mid + 1;
+                else
                 return nums[mid];
-                else if(mid+1<nums.size() && mid-1>=0 && nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
+            }
+            else if(mid == n-1){
+                if(nums[mid]==nums[mid-1])
+                h = mid-1;
+                else
                 return nums[mid];
-                else if(nums[mid]==nums[mid+1])
+            }
+            else if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+            return nums[mid];
+            else if(mid%2 == 0){
+                if(nums[mid]==nums[mid+1])
                 l = mid+1;
+                else
+                h = mid-1;
+            }
+            else{
+                if(nums[mid]==nums[mid-1])
+                l = mid + 1;
                 else
                 h = mid - 1;
             }
-            else{
-                if(mid-1>=0 && nums[mid]==nums[mid-1])
-                l = mid + 1;
-                else
-                h = mid -1 ;
-            }
         }
-        return -1;
+        return nums[0];
     }
 };
