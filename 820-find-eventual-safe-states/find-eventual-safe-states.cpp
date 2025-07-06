367770d91ee2ab7687806fb23dd40effaa6e5c73
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-07-06 14:15:16 +0530

    Time: 47 ms (55.09%) | Memory: 64 MB (56.31%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<int>adj[V];
-         vector<int>outdegree(V,0);
-        for(int i=0;i<V;i++){
+        int n = graph.size();
+        vector<int>outdegree(n,0);
+        int node = 0;
+        for(auto &it:graph){
+            int sz = it.size();
+            outdegree[node]=(sz);
+            node++;
+        }
+        vector<int>adj[n];
+        for(int i=0;i<n;i++){
             for(int j=0;j<graph[i].size();j++){
                 adj[graph[i][j]].push_back(i);
             }
-            outdegree[i] = graph[i].size();
         }
-       queue<int>q;
-       for(int i=0;i<V;i++){
+
+        queue<int>q;
+        vector<int>visited(n,0);
+        for(int i=0;i<n;i++){
+            if(outdegree[i]==0){
+                q.push(i);
+                // visited[i] = 1;
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            // ans.push_back(node);
+            for(auto it:adj[node]){
+                outdegree[it]--;
+                if(outdegree[it]==0){
+                    q.push(it);
+                    // visited[it] = 1;
+                }
+            }
+        }
+      for(int i=0;i<n;i++){
         if(outdegree[i]==0)
-        q.push(i);
-       }
-       vector<int>safe(V,0);
-       while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        safe[node] = 1;
-        for(auto it:adj[node]){
-            outdegree[it]--;
-            if(outdegree[it]==0)
-            q.push(it);
-        }
-       }
-       vector<int>ans;
-       for(int i=0;i<V;i++){
-        if(safe[i])
         ans.push_back(i);
-       }
-       return ans;
+      }
+        return ans;
+
     }
 };
