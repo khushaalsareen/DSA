206795fea0e2f6041ba5f7a1a87535da1fe078b1
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-07-07 23:17:22 +0530

    Time: 3 ms (62.59%) | Memory: 118 MB (69.38%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -9,42 +9,42 @@
  * };
  */
 class Solution {
-public:
-    ListNode * getMiddle(ListNode*head){
-        ListNode * slow = head;
-        ListNode * fast = head;
+    ListNode * reverse(ListNode * head){
         ListNode * prev = NULL;
-        while(fast && fast->next && fast->next->next){
-            prev = slow;
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
-    ListNode* reverse(ListNode*head){
         ListNode * curr = head;
-        ListNode * prev = NULL;
         while(curr){
-            ListNode * tmp = curr->next;
+            ListNode*tmp = curr->next;
             curr->next = prev;
             prev = curr;
             curr = tmp;
         }
         return prev;
     }
+
+    ListNode * getMiddle(ListNode * head){
+        ListNode * slow = head;
+        ListNode * fast = head;
+        while(fast && fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+public:
     bool isPalindrome(ListNode* head) {
         if(!head || !head->next)
         return true;
-        ListNode * mid = getMiddle(head);
-        ListNode * h2 = mid->next;
-        mid->next = NULL;
-        h2 = reverse(h2);
-        ListNode*h1 = head;
-        while(h1 && h2){
-            if(h1->val != h2->val)
+
+        ListNode * middle = getMiddle(head);
+        ListNode * reversedHead = reverse(middle->next);
+        middle->next = NULL;
+        ListNode * tmp1 = head;
+        ListNode * tmp2 = reversedHead;
+        while(tmp1 && tmp2){
+            if(tmp1->val !=tmp2->val)
             return false;
-            h1 = h1->next;
-            h2 = h2->next;
+            tmp1 = tmp1->next;
+            tmp2 = tmp2->next;
         }
         return true;
     }
