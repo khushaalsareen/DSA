e27b2b3ae9494458f1669789f0640ee86b6a0a8e
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-07-07 23:31:31 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (88.04%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,39 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        int cnt = 1;
-        ListNode*temp1=NULL,*temp2=NULL,*head1=NULL,*head2=NULL;
-        if(!head) return NULL;
-        if(!head->next) return head;
-        ListNode*temp = head;
-        while(temp){
-            if(cnt%2 == 0) // even
-            {
-                if(temp2==NULL){
-                temp2 = temp;
-                head2 = temp;
+        if(!head || !head->next)
+        return head;
+
+        ListNode * oddHead = NULL;
+        ListNode * evenHead = NULL;
+        ListNode * oddTail = oddHead;
+        ListNode * evenTail = evenHead;
+        ListNode * tmp = head;
+        bool flg = true;
+        while(tmp){
+            if(flg){
+                // odd index node
+                if(oddHead == NULL){
+                    oddHead = tmp;
+                    oddTail = oddHead;
                 }
                 else{
-                temp2->next = temp;
-                temp2 = temp2->next;
+                    oddTail->next = tmp;
+                    oddTail = oddTail->next;
                 }
             }
-            else {
-                if(temp1==NULL){
-                temp1 = temp;
-                head1 = temp;
+            else{
+                if(evenHead == NULL){
+                    evenHead = tmp;
+                    evenTail = evenHead;
                 }
                 else{
-                temp1->next = temp;
-                temp1 = temp1->next;
+                    evenTail->next = tmp;
+                    evenTail = evenTail->next;
                 }
             }
-            temp = temp->next;
-            cnt+=1;
+            flg = flg==true? false: true;
+            tmp = tmp->next;
         }
-        temp1->next = NULL;
-        temp2->next = NULL;
-        temp1->next = head2;
-        return head1;
+        oddTail->next = NULL;
+        evenTail->next = NULL;
+        oddTail->next = evenHead;
+        return oddHead;
     }
 };
