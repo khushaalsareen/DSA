07cc90b2edb7b46cbe0ed19e88b3dbbce27acea4
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-07-18 01:19:53 +0530

    Time: 19 ms (34.53%) | Memory: 9.3 MB (70.54%) - LeetSync

--- 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.cpp
+++ 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.cpp
@@ -1,40 +1,72 @@
 class Solution {
-public:
-    int f(int ind, int k, string &str, vector<vector<int>> &dp, vector<vector<int>> &memo) {
-        if (k == 0)
-            return (ind == -1) ? 0 : 1e9;  // If no characters remain but we still need partitions, return high cost.
-        if (ind == -1)
-            return 1e9;  // If no characters remain but partitions are still needed.
-
-        if (memo[ind][k] != -1)
-            return memo[ind][k];
-
-        int ans = 1e9;
-        for (int j = ind; j >= 0; j--) {
-            int steps = dp[j][ind] + f(j - 1, k - 1, str, dp, memo);
-            ans = min(ans, steps);
+    bool isPalindrome(int i, int j, string &s){
+        while(i<j)
+        {
+            if(s[i]!=s[j])
+            return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    int f(int ind, int k, string &s, vector<vector<int>>&dp){
+        if(ind == s.size()){
+            if(k==0)
+            return 0;
+            else
+            return 1e9;
         }
 
-        return memo[ind][k] = ans;
-    }
-
-    int palindromePartition(string str, int k) {
-        int sz = str.size();
-        vector<vector<int>> dp(sz, vector<int>(sz, 0));
-        vector<vector<int>> memo(sz, vector<int>(k + 1, -1)); // Memoization table for recursive function.
-
-        // Precompute the costs to convert substrings into palindromes.
-        for (int len = 2; len <= sz; len++) {
-            for (int i = 0; i + len - 1 < sz; i++) {
-                int j = i + len - 1;
-                if (str[i] == str[j])
-                    dp[i][j] = dp[i + 1][j - 1];
-                else
-                    dp[i][j] = 1 + dp[i + 1][j - 1];
+        if(k==1)
+        {
+            bool check = isPalindrome(ind,s.size()-1,s);
+            if(check)
+            return 0;
+            else
+            {
+                int i = ind;
+                int j = s.size() - 1;
+                int tot = 0;
+                while(i<j){
+                    if(s[i]!=s[j])
+                    tot++;
+                    i++;
+                    j--;
+                }
+                return tot;
             }
         }
 
-        // Call the recursive function.
-        return f(sz - 1, k, str, dp, memo);
+        if(dp[ind][k]!=-1)
+        return dp[ind][k];
+
+        int ans = 1e9;
+        for(int j = ind;j<s.size();j++){
+            bool check = isPalindrome(ind,j,s);
+            int curr = 0;
+            if(check){
+                curr =  0;
+            }
+            else{
+                int i = ind;
+                int k = j;
+                int tot = 0;
+                while(i<k){
+                    if(s[i]!=s[k])
+                    tot++;
+                    i++;
+                    k--;
+                }
+                curr = tot;
+            }
+            curr +=f(j+1,k-1,s,dp);
+            ans  = min(ans,curr);
+        }
+        return dp[ind][k]= ans;
+    }
+public:
+    int palindromePartition(string s, int k) {
+        vector<vector<int>>dp(s.size()+1,vector<int>(k+1,-1));
+        return f(0,k,s,dp);
     }
 };
