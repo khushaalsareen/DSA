3553dee741159d2023d6c0e9f24825a6692fc21a
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-07-18 00:43:28 +0530

    Time: 3 ms (89.89%) | Memory: 20.6 MB (43.99%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,28 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-    unordered_map<long, int> map;
-    int count = 0;
-
-    void countPathSum(TreeNode* root, int target, long sum) {
-        if (!root)
-            return;
-        sum += root->val; // Path sum from root
-        if (sum == target)
-            count++;
-        if (map.find(sum - target) !=
-            map.end()) // checking whether any target sum path present in the
-                       // path from root to the current node
-            count += map[sum - target];
-        map[sum]++;
-        countPathSum(root->left, target, sum);
-        countPathSum(root->right, target, sum);
-        map[sum]--; // After visiting the left and right subtree, we have to
-                    // reduce this path sum count from map since we are leaving
-                    // this path
+    int cnt = 0;
+    unordered_map<long int,int>m;
+    
+    void f(TreeNode * root, long int psum, int target){
+        if(!root)
+        return;
+        psum+=root->val;
+        if(m.find(psum-target)!=m.end())
+        cnt+=m[psum-target];
+        m[psum]++;
+        f(root->left,psum,target);
+        f(root->right,psum,target);
+        m[psum]--;
     }
-
+public:
     int pathSum(TreeNode* root, int targetSum) {
-        countPathSum(root, targetSum, 0);
-        return count;
+        if(!root)
+        return 0;
+        m[0] = 1;
+        f(root,0*1LL,targetSum);
+        return cnt;
     }
 };
