507ee22e361e0c30518f2e80e15f8dcd7d811485
Khushaal Sareen <99650568+khushaalsareen@users.noreply.github.com>
2025-07-18 00:08:41 +0530

    Time: 4 ms (81.99%) | Memory: 20.7 MB (35.21%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,39 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void f(TreeNode*root, long long int &sum, int target, int &cnt){
-        if(!root)
-        return;
-        sum=sum + root->val;
-        if(sum==target)
-        cnt++;
-        f(root->left,sum,target,cnt);
-        f(root->right,sum,target,cnt);
-        sum-=root->val;
+    unordered_map<long, int> map;
+    int count = 0;
+
+    void countPathSum(TreeNode* root, int target, long sum) {
+        if (!root)
+            return;
+        sum += root->val; // Path sum from root
+        if (sum == target)
+            count++;
+        if (map.find(sum - target) !=
+            map.end()) // checking whether any target sum path present in the
+                       // path from root to the current node
+            count += map[sum - target];
+        map[sum]++;
+        countPathSum(root->left, target, sum);
+        countPathSum(root->right, target, sum);
+        map[sum]--; // After visiting the left and right subtree, we have to
+                    // reduce this path sum count from map since we are leaving
+                    // this path
     }
-    void traverse(TreeNode*root, int &cnt, int target){
-        if(!root)
-        return;
-        long long int sum = 0;
-        f(root,sum,target,cnt);
-        traverse(root->left,cnt,target);
-        traverse(root->right,cnt,target);
-    }
+
     int pathSum(TreeNode* root, int targetSum) {
-        int cnt = 0;
-        if(!root)
-        return 0;
-        traverse(root,cnt,targetSum);
-        return cnt;
+        countPathSum(root, targetSum, 0);
+        return count;
     }
 };
